b6ef25dfd91c3aa60c17e3b85f9b2c1a7aecf291
Alejandro Revilla <apr@jpos.org>
1999-03-03 17:38:06 +0000

    ISORequestListeners should be Observable

--- jpos/src/main/org/jpos/iso/ISORequestListener.java
+++ jpos/src/main/org/jpos/iso/ISORequestListener.java
@@ -1,5 +1,7 @@
 package uy.com.cs.jpos.iso;
 
+import java.util.*;
+
 /**
  * It is possible to ask ISOMUX to forward all unmatched
  * messages received through its associated ISOChannel
@@ -14,4 +16,5 @@ public interface ISORequestListener {
 	 * @param	m	the unmatched request
 	 */
 	public void process(ISOMsg m);
+	public void addObserver(Observer o);
 }
