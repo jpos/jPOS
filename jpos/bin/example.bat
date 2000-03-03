9c99f5e79a9eba48d8dd14e88fbec1e5acc7283d
Alejandro Revilla <apr@jpos.org>
2000-03-03 11:50:24 +0000

    Added to repository

---
+++ jpos/bin/example.bat
@@ -0,0 +1,12 @@
+@echo off
+REM $Id$
+set JAVA=%JAVA_HOME%\bin\java
+set CLASSPATH=build\classes;build\examples;%CLASSPATH%
+set cp=%CLASSPATH%
+for %%i in (lib\*.jar) do call cp.bat %%i
+for %%i in (ext\*.jar) do call cp.bat %%i
+set CP=%CP%;%JAVA_HOME%\lib\tools.jar
+
+%JAVA% -classpath %CP% %1.Test %2 %3 %4 %5 %6
+
+
