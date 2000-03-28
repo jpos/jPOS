5cd5fa2f93ff39d5a60cd1ae5c2f14cc78f38e59
Alejandro Revilla <apr@jpos.org>
2000-03-28 00:52:51 +0000

    Added LogEvent parameter

--- jpos/src/main/org/jpos/iso/ISOFilter.java
+++ jpos/src/main/org/jpos/iso/ISOFilter.java
@@ -1,5 +1,7 @@
 package org.jpos.iso;
 
+import org.jpos.util.LogEvent;
+
 /**
  * An ISOFilter has the oportunity to modify an incoming or
  * outgoing ISOMsg that is about to go thru an ISOChannel.
@@ -8,19 +10,27 @@ package org.jpos.iso;
  * @version $Revision$ $Date$
  */
 public interface ISOFilter {
-    public class VetoException extends Exception {
+    public class VetoException extends ISOException {
 	public VetoException () {
 	    super();
 	}
 	public VetoException (String detail) {
 	    super(detail);
 	}
+	public VetoException (Exception nested) {
+	    super(nested);
+	}
+	public VetoException (String detail, Exception nested) {
+	    super(detail, nested);
+	}
     }
     /**
      * @param channel current ISOChannel instance
      * @param m ISOMsg to filter
+     * @param evt LogEvent
      * @return an ISOMsg (possibly parameter m)
      * @throws VetoException
      */
-    public ISOMsg filter (ISOChannel channel, ISOMsg m) throws VetoException;
+    public ISOMsg filter (ISOChannel channel, ISOMsg m, LogEvent evt) 
+	throws VetoException;
 }
