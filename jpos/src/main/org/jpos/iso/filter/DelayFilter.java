caa395be5e31921c5b873d91ffa3aa3ab1dddfa1
Alejandro Revilla <apr@jpos.org>
2000-04-14 17:02:26 +0000

    Added DelayFilter

---
+++ jpos/src/main/org/jpos/iso/filter/DelayFilter.java
@@ -0,0 +1,50 @@
+package org.jpos.iso.filter;
+
+import org.jpos.iso.ISOFilter;
+import org.jpos.iso.ISOMsg;
+import org.jpos.iso.ISOChannel;
+import org.jpos.util.LogEvent;
+import org.jpos.core.Configurable;
+import org.jpos.core.Configuration;
+
+
+/**
+ * DelayFilter can be used in order to
+ * slow down an ISOChannel. Usefull while
+ * debugging an application or simulating a server
+ */
+public class DelayFilter implements ISOFilter, Configurable {
+    int delay;
+    public DelayFilter() {
+	super();
+	delay = 0;
+    }
+   /**
+    * @param delay desired delay, expressed in milliseconds
+    */
+    public DelayFilter(int delay) {
+	super();
+	this.delay = delay;
+    }
+   /**
+    * @param cfg
+    * <ul>
+    * <li>delay (expressed in milliseconds)
+    * </ul>
+    */
+    public void setConfiguration (Configuration cfg) {
+	delay = cfg.getInt ("delay");
+    }
+
+    public ISOMsg filter (ISOChannel channel, ISOMsg m, LogEvent evt) 
+    {
+	evt.addMessage ("<delay-filter delay=\""+delay+"\"/>");
+	if (delay > 0) {
+	    try {
+		Thread.sleep (delay);
+	    } catch (InterruptedException e) { }
+	}
+	return m;
+    }
+}
+
