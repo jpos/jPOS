08742fe4694e20a24e1eb376e2398dc19b272e56
Alejandro Revilla <apr@jpos.org>
2000-04-14 12:10:32 +0000

    Added to repository

---
+++ jpos/src/main/org/jpos/core/Configurable.java
@@ -0,0 +1,13 @@
+package org.jpos.core;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author <a href="mailto:apr@cs.com.uy">Alejandro P. Revilla</a>
+ * @version $Revision$ $Date$
+ * @since jPOS 1.2
+ */
+public interface Configurable {
+    public void setConfiguration (Configuration cfg);
+}
