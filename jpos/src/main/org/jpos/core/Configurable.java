75a5878f7627c9d552175668c2fc2addd0ca99c6
Alejandro Revilla <apr@jpos.org>
2000-04-14 12:31:45 +0000

    Configurable can throw an ISOException

--- jpos/src/main/org/jpos/core/Configurable.java
+++ jpos/src/main/org/jpos/core/Configurable.java
@@ -2,6 +2,7 @@ package org.jpos.core;
 
 import java.io.*;
 import java.util.*;
+import org.jpos.iso.ISOException;
 
 /**
  * @author <a href="mailto:apr@cs.com.uy">Alejandro P. Revilla</a>
@@ -9,5 +10,5 @@ import java.util.*;
  * @since jPOS 1.2
  */
 public interface Configurable {
-    public void setConfiguration (Configuration cfg);
+    public void setConfiguration (Configuration cfg) throws ISOException;
 }
