786c45ff8f8cfb9b88e804d94d31948ba02d2498
Alejandro Revilla <apr@jpos.org>
2000-04-15 23:35:01 +0000

    throw ConfigurationException instead of ISOException

--- jpos/src/main/org/jpos/core/Configurable.java
+++ jpos/src/main/org/jpos/core/Configurable.java
@@ -5,10 +5,16 @@ import java.util.*;
 import org.jpos.iso.ISOException;
 
 /**
+ * Object is Configurable
  * @author <a href="mailto:apr@cs.com.uy">Alejandro P. Revilla</a>
  * @version $Revision$ $Date$
  * @since jPOS 1.2
  */
 public interface Configurable {
-    public void setConfiguration (Configuration cfg) throws ISOException;
+   /**
+    * @param cfg Configuration object
+    * @throws ConfigurationException
+    */
+    public void setConfiguration (Configuration cfg)
+	throws ConfigurationException;
 }
