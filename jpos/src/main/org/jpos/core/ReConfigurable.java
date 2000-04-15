0b105cc5c2e727464928e2e619f6961d8fd1721d
Alejandro Revilla <apr@jpos.org>
2000-04-15 23:35:28 +0000

    some day we will have ReConfigurable components...

---
+++ jpos/src/main/org/jpos/core/ReConfigurable.java
@@ -0,0 +1,15 @@
+package org.jpos.core;
+
+import java.io.*;
+import java.util.*;
+import org.jpos.iso.ISOException;
+
+/**
+ * Tag interface, indicates that object may be reconfigured
+ * @author <a href="mailto:apr@cs.com.uy">Alejandro P. Revilla</a>
+ * @version $Revision$ $Date$
+ * @since jPOS 1.2
+ */
+public interface ReConfigurable extends Configurable {
+}
+
