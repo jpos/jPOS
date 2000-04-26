01c1d4bce8502f633787b20829d9d6ddb5fab542
Alejandro Revilla <apr@jpos.org>
2000-04-26 00:50:36 +0000

    ISOServer now Observable

--- jpos/src/ext/org/jpos/apps/qsp/config/ConfigServer.java
+++ jpos/src/ext/org/jpos/apps/qsp/config/ConfigServer.java
@@ -1,11 +1,13 @@
 package org.jpos.apps.qsp.config;
 
 import java.util.Properties;
+import javax.swing.JPanel;
 
 import org.jpos.iso.ISOChannel;
 import org.jpos.iso.ServerChannel;
 import org.jpos.iso.ISOException;
 import org.jpos.iso.ISORequestListener;
+import org.jpos.iso.gui.ISOChannelPanel;
 import org.jpos.core.Configurable;
 import org.jpos.core.SimpleConfiguration;
 import org.jpos.core.ConfigurationException;
@@ -56,6 +58,12 @@ public class ConfigServer implements QSPConfigurator {
 	evt.addMessage ("Server "+name+"/"+channel.getName()+"/"+port);
 	server.setName (name);
 	server.setLogger (logger, realm);
+	JPanel panel = ConfigControlPanel.getPanel (node);
+	if (panel != null) {
+	    ISOChannelPanel icp = new ISOChannelPanel (name);
+	    panel.add (icp);
+	    server.addObserver (icp);
+	}
 	new Thread (server).start();
 	Logger.log (evt);
     }
