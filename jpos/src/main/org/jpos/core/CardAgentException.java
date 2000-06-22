40a11d17e4ba7b4b6cabff91b6fb7e8c3fa4baec
Alejandro Revilla <apr@jpos.org>
2000-06-22 15:02:32 +0000

    CardAgentException now extends ISOException

--- jpos/src/main/org/jpos/core/CardAgentException.java
+++ jpos/src/main/org/jpos/core/CardAgentException.java
@@ -1,35 +1,12 @@
-/*
- * $Log$
- * Revision 1.5  2000/03/02 12:31:01  apr
- * Get rid of javadoc warnings - done
- *
- * Revision 1.4  2000/03/01 14:44:38  apr
- * Changed package name to org.jpos
- *
- * Revision 1.3  1999/10/08 12:53:56  apr
- * Devel intermediate version - CVS sync
- *
- * Revision 1.2  1999/09/26 22:31:56  apr
- * CVS sync
- *
- * Revision 1.1  1999/09/26 19:54:03  apr
- * jPOS core 0.0.1 - setting up artifacts
- *
- */
-
 package org.jpos.core;
+import org.jpos.iso.ISOException;
 
 /**
  * @author apr@cs.com.uy
  * @version $Id$
  * @since jPOS 1.1
  */
-public class CardAgentException extends Exception {
-    /**
-     * @serial
-     */
-    Exception e = null;
-
+public class CardAgentException extends ISOException {
     public CardAgentException () {
 	super();
     }
@@ -37,20 +14,12 @@ public class CardAgentException extends Exception {
 	super(s);
     }
     public CardAgentException (Exception e) {
-	super();
-	this.e = e;
+	super(e);
     }
     public CardAgentException (String s, Exception e) {
-	super(s);
-	this.e = e;
+	super(s, e);
     }
-    public Exception getNested() {
-	return e;
-    }
-    public String toString() {
-	StringBuffer buf = new StringBuffer (super.toString());
-	if (e != null)
-	    buf.append (" (" + e.toString() +")");
-	return buf.toString();
+    protected String getTagName() {
+	return "card-agent-exception";
     }
 }
