c4bddf8e5736c7c57f8cb66ad6a0ca24711cd55c
Alejandro Revilla <apr@jpos.org>
2000-08-14 01:13:17 +0000

    Mux is now Reconfigurable

--- jpos/src/ext/org/jpos/apps/qsp/config/ConfigMux.java
+++ jpos/src/ext/org/jpos/apps/qsp/config/ConfigMux.java
@@ -16,6 +16,7 @@ import org.jpos.util.NameRegistrar.NotFoundException;
 
 import org.jpos.apps.qsp.QSP;
 import org.jpos.apps.qsp.QSPConfigurator;
+import org.jpos.apps.qsp.QSPReConfigurator;
 
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -26,20 +27,42 @@ import org.w3c.dom.NamedNodeMap;
  * @author <a href="mailto:apr@cs.com.uy">Alejandro P. Revilla</a>
  * @version $Revision$ $Date$
  */
-public class ConfigMux implements QSPConfigurator {
+public class ConfigMux implements QSPReConfigurator {
     public void config (QSP qsp, Node node) throws ConfigurationException
     {
-	LogEvent evt = new LogEvent (qsp, "config-mux");
 	String name = node.getAttributes().getNamedItem ("name").getNodeValue();
+	LogEvent evt = new LogEvent (qsp, "config-mux", name);
 	Logger logger = ConfigLogger.getLogger (node);
 	String realm  = ConfigLogger.getRealm (node);
 	ISOChannel channel = ConfigChannel.getChildChannel (node);
 	ISOMUX mux = new ISOMUX (channel, logger, realm);
+	boolean connect = node.getAttributes()
+			.getNamedItem("connect").getNodeValue().equals("yes");
 	evt.addMessage ("MUX "+name+"/"+channel.getName());
 	mux.setName (name);
+	mux.setConnect (connect);
 	new Thread (mux).start();
 	Logger.log (evt);
     }
+    public void reconfig (QSP qsp, Node node) throws ConfigurationException
+    {
+	String name = node.getAttributes().getNamedItem ("name").getNodeValue();
+	LogEvent evt = new LogEvent (qsp, "re-config-mux", name);
+	boolean connect = node.getAttributes()
+			.getNamedItem("connect").getNodeValue().equals("yes");
+	try {
+	    ISOMUX mux = ISOMUX.getMUX (name);
+	    mux.setLogger (ConfigLogger.getLogger (node),
+			   ConfigLogger.getRealm (node));
+	    mux.setConnect (connect);
+	} catch (NotFoundException e) {
+	    evt.addMessage (e);
+	    throw new ConfigurationException (e);
+	} finally {
+	    Logger.log (evt);
+	}
+    }
+
     public static ISOMUX getMUX (Node node) {
 	Node n = node.getAttributes().getNamedItem ("name");
 	if (n != null)
