5dc3d10e64ecb7f2a55a9a352705b6791c98b980
Alejandro Revilla <apr@jpos.org>
2000-08-16 23:23:54 +0000

    Simplified CardAgent interface (no getResponse and serialization stuff)

--- jpos/src/main/org/jpos/core/CardAgent.java
+++ jpos/src/main/org/jpos/core/CardAgent.java
@@ -1,31 +1,3 @@
-/*
- * $Log$
- * Revision 1.8  2000/07/19 00:51:23  apr
- * removed cancel method
- *
- * Revision 1.7  2000/06/21 20:43:36  apr
- * Added PersistentEngine support
- *
- * Revision 1.6  2000/03/01 14:44:38  apr
- * Changed package name to org.jpos
- *
- * Revision 1.5  2000/01/30 23:33:49  apr
- * CVS sync/backup - intermediate version
- *
- * Revision 1.4  1999/11/26 12:16:44  apr
- * CVS devel snapshot
- *
- * Revision 1.3  1999/10/08 12:53:56  apr
- * Devel intermediate version - CVS sync
- *
- * Revision 1.2  1999/09/26 22:31:55  apr
- * CVS sync
- *
- * Revision 1.1  1999/09/26 19:54:02  apr
- * jPOS core 0.0.1 - setting up artifacts
- *
- */
-
 package org.jpos.core;
 
 import java.io.*;
@@ -34,7 +6,7 @@ import org.jpos.tpl.PersistentEngine;
 
 /**
  * @author apr@cs.com.uy
- * @version $Id$
+ * @version $Revision$ $Date$
  * @since jPOS 1.1
  *
  * Implements financial institution specific functionality<br>
@@ -54,19 +26,9 @@ public interface CardAgent {
 
     /**
      * @param t CardTransaction
-     * @return true if agent is able to handle this transaction
+     * @return true if agent is able/willing to handle this transaction
      */
     public boolean canHandle (CardTransaction t);
-
-    /**
-     * create a CardTransactionResponse based on a previously
-     * serialized image
-     * @param b agent generated image
-     * @return CardTransactionResponse
-     * @exception CardAgentException
-     */
-    public CardTransactionResponse getResponse (byte[] b) 
-	throws CardAgentException;
 
     /**
      * Process the transaction
@@ -83,18 +45,11 @@ public interface CardAgent {
     public String getPropertyPrefix();
 
     /**
-     * Process a batch of previously completed transactions (close batch)
-     * @param l List of pre-aprooved transactions
-     * @return List of actually closed transactions
-     * @exception CardAgentException
-    public List processBatch (List l) throws CardAgentException;
-     */
-
-    /**
      * Set PersistentEngine associated with this CardAgent
      * @param engine a PersistentEngine instance
      */
     public void setPersistentEngine (PersistentEngine engine);
+
     /**
      * @return PersistentEngine instance
      */
