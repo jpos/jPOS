54d6eced8dad31ac80c6fc13082fb0df8da3c490
Alejandro Revilla <apr@jpos.org>
2000-12-26 12:15:42 +0000

    Try mti_proccode class before mti

--- jpos/src/ext/org/jpos/apps/qsp/RequestDispatcher.java
+++ jpos/src/ext/org/jpos/apps/qsp/RequestDispatcher.java
@@ -85,9 +85,12 @@ public class RequestDispatcher
     }
     public boolean process (ISOSource source, ISOMsg m) {
         try {
-            String mti = m.getMTI();
+            String mti      = m.getMTI();
+            String proccode = m.hasField(3) ? m.getString(3) : "";
             String className = cfg.get("prefix", "") + mti;
-            Class c = Class.forName(className);
+            Class c = Class.forName(className + "_" + proccode);
+            if (c == null)
+                c = Class.forName(className);
             if (c != null) {
                 ISORequestListener rl = (ISORequestListener) c.newInstance();
                 if (rl instanceof LogSource)
