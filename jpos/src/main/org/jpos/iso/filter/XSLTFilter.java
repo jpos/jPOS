ddafc053418ad99b8a5c004b5fdc7c026670a657
Alejandro Revilla <apr@jpos.org>
2001-02-03 23:40:07 +0000

    Upgrade to Xalan 2.0.0 (we use TRAX now)

--- jpos/src/main/org/jpos/iso/filter/XSLTFilter.java
+++ jpos/src/main/org/jpos/iso/filter/XSLTFilter.java
@@ -52,20 +52,17 @@ package org.jpos.iso.filter;
 import java.io.*;
 import org.jpos.iso.*;
 import org.jpos.iso.packager.*;
-import org.xml.sax.SAXException;
-import org.apache.xalan.xpath.XPathException;
-import org.apache.xalan.xslt.XSLTProcessorFactory;
-import org.apache.xalan.xslt.XSLTInputSource;
-import org.apache.xalan.xslt.XSLTResultTarget;
-import org.apache.xalan.xslt.XSLTProcessor;
-import org.apache.xalan.xpath.XString;
-
 
 import org.jpos.util.LogEvent;
 import org.jpos.core.Configurable;
 import org.jpos.core.Configuration;
 import org.jpos.core.ConfigurationException;
 import org.jpos.iso.ISOFilter.VetoException;
+
+import javax.xml.transform.*;
+import javax.xml.transform.sax.*;
+import javax.xml.transform.dom.*;
+import javax.xml.transform.stream.*;
 
 /**
  * Implements ISOFilter by means of XSL-Transformations
@@ -75,36 +72,40 @@ import org.jpos.iso.ISOFilter.VetoException;
 public class XSLTFilter implements ISOFilter, Configurable {
     boolean reread;
     String xsltfile;
-    XSLTProcessor processor;
-    XSLTInputSource xslt;
+    TransformerFactory tfactory;
+    Transformer transformer;
     XMLPackager packager;
 
     /**
      * Default noargs constructor
-     * @throws SAXException
      * @throws ISOException
      */
-    public XSLTFilter () throws ISOException, SAXException  {
+    public XSLTFilter () throws ISOException {
 	super();
-	packager = new XMLPackager();
-	processor = XSLTProcessorFactory.getProcessor();
-        xsltfile  = null;
-	reread    = true;
+	packager    = new XMLPackager();
+        tfactory    = TransformerFactory.newInstance();
+        transformer = null;
+	reread      = true;
     }
 
     /**
      * @param xsltfile XSL Transformation file
      * @param reread true if you want XSLT file re-read from disk
-     * @throws SAXException
      * @throws ISOException
      */
     public XSLTFilter (String xsltfile, boolean reread) 
-	throws ISOException, SAXException
+	throws ISOException
     {
-	this();
-	this.xsltfile = xsltfile;
-	this.reread   = reread;
-	xslt = new XSLTInputSource(xsltfile);
+        this();
+        try {
+	    this.xsltfile    = xsltfile;
+	    this.reread      = reread;
+	    this.transformer = 
+                tfactory.newTransformer(new StreamSource(xsltfile));
+        } catch (TransformerConfigurationException e) {
+            throw new ISOException (e);
+        }
+
     }
 
    /**
@@ -118,7 +119,9 @@ public class XSLTFilter implements ISOFilter, Configurable {
 	throws ConfigurationException
     {
 	try {
-	    xslt     = new XSLTInputSource(cfg.get("xsltfile"));
+	    transformer = tfactory.newTransformer(
+                new StreamSource(cfg.get("xsltfile"))
+            );
 	    String s = cfg.get ("reread");
 	    reread   =  (s == null || s.equals ("no"));
 	} catch (Exception e) {
@@ -140,18 +143,16 @@ public class XSLTFilter implements ISOFilter, Configurable {
 	    m.setPackager (packager);
 	    ByteArrayOutputStream os = new ByteArrayOutputStream();
 
-	    if (reread || xslt == null)
-		xslt = new XSLTInputSource(xsltfile);
-
-	    processor.process(
-		new XSLTInputSource(new ByteArrayInputStream (m.pack())),
-		xslt, 
-		new XSLTResultTarget(os)
-	    );
+	    if (reread || transformer == null)
+		transformer = tfactory.newTransformer(
+                    new StreamSource(xsltfile)
+                );
+	    transformer.transform (
+                new StreamSource(new ByteArrayInputStream (m.pack())),
+                new StreamResult(os)
+            );
 	    m.unpack (os.toByteArray());
-	} catch (SAXException e) {
-	    throw new VetoException(e);
-	} catch (ISOException e) {
+	} catch (Exception e) {
 	    throw new VetoException(e);
 	}
 	return m;
