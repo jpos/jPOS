338193eef4f8f171d0a14b10eafef58e80c39592
Alejandro Revilla <apr@jpos.org>
2001-04-16 20:32:21 +0000

    Contributed example.bat (lzabalet@adinet.com.uy) (CRLF stuff)

--- jpos/bin/example.bat
+++ jpos/bin/example.bat
@@ -1,12 +1,11 @@
 @echo off
 REM $Id$
 set JAVA=%JAVA_HOME%\bin\java
-set CLASSPATH=build\classes;build\examples;%CLASSPATH%
-set cp=%CLASSPATH%
+set cp=build\classes;build\examples;%CLASSPATH%
 for %%i in (ext\*.jar) do call cp.bat %%i
 for %%i in (lib\*.jar) do call cp.bat %%i
 set CP=%CP%;%JAVA_HOME%\lib\tools.jar
 
 %JAVA% -classpath %CP% -Djpos.config=src/etc/jpos.cfg -Dsax.parser=org.apache.xerces.parsers.SAXParser %1.Test %2 %3 %4 %5 %6
 
-
+set cp=
