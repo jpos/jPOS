ccdec38648f04e7e7aa5d513f4cca9fb3ba3f3dd
Alejandro Revilla <apr@jpos.org>
2001-04-22 00:46:34 +0000

    Added getPayLoad()

--- jpos/src/main/org/jpos/util/LogEvent.java
+++ jpos/src/main/org/jpos/util/LogEvent.java
@@ -53,45 +53,6 @@ import java.io.*;
 import java.util.*;
 import java.sql.SQLException;
 
-
-/*
- * $Log$
- * Revision 1.7  2000/11/02 12:09:18  apr
- * Added license to every source file
- *
- * Revision 1.6  2000/08/09 12:10:02  apr
- * Dump stacktrace on SQLExceptions
- *
- * Revision 1.5  2000/04/17 11:19:45  apr
- * BugFix: ending tag in addMessage(String,String) -
- * Thanks to Arun Kumar U <bksys@vsnl.com> !
- *
- * Revision 1.4  2000/04/16 23:53:14  apr
- * LogProducer renamed to LogSource
- *
- * Revision 1.3  2000/03/02 12:31:10  apr
- * Get rid of javadoc warnings - done
- *
- * Revision 1.2  2000/03/01 14:44:45  apr
- * Changed package name to org.jpos
- *
- * Revision 1.1  2000/01/11 01:24:57  apr
- * moved non ISO-8583 related classes from jpos.iso to jpos.util package
- * (AntiHog LeasedLineModem LogEvent LogListener LogSource
- *  Loggeable Logger Modem RotateLogListener SimpleAntiHog SimpleDialupModem
- *  SimpleLogListener SimpleLogSource SystemMonitor V24)
- *
- * Revision 1.4  1999/11/24 18:04:39  apr
- * minor DOC changes
- *
- * Revision 1.3  1999/11/18 23:39:03  apr
- * Added addMessage(tag,message) and SQLException support
- *
- * Revision 1.2  1999/09/21 21:33:19  apr
- * LogEvent now extends EventObject and LogListener is an EventListener
- *
- */
-
 /**
  * @author apr@cs.com.uy
  * @version $Id$
@@ -166,6 +127,9 @@ public class LogEvent extends EventObject {
     }
     public String getRealm() {
 	return source.getRealm();
+    }
+    public Vector getPayLoad() {
+        return payLoad;
     }
 }
 
