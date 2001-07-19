e60cea70a376b0c3476055c51637f68e5e54a786
Alejandro Revilla <apr@jpos.org>
2001-07-19 18:42:03 +0000

    Fixed broken example.bat (thanks to Hani Kirollos <hani_kirollos@yahoo.com>)

--- jpos/bin/example.bat
+++ jpos/bin/example.bat
@@ -1,8 +1,8 @@
 @echo off
 REM $Id$
 set JAVA=%JAVA_HOME%\bin\java
-set cp=build\classes:build\examples;lib/xerces_1_2_3.jar;lib/xalan_2_0_0.jar;lib/log4j.jar;%CLASSPATH%
-for %%i in (ext\*.jar) do call cp.bat %%i
+set cp=build\classes;build\examples;lib\xerces_1_2_3.jar;lib\xalan_2_0_0.jar;lib\log4j.jar;%CLASSPATH%
+for %%i in (ext\*.jar) do call bin\cp.bat %%i
 set CP=%CP%;%JAVA_HOME%\lib\tools.jar
 
 %JAVA% -classpath %CP% -Djpos.config=src/etc/jpos.cfg -Dsax.parser=org.apache.xerces.parsers.SAXParser %1.Test %2 %3 %4 %5 %6
