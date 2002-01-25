7a19ecc13119f6adfe89c7a81407bcd67aab632b
Alejandro Revilla <apr@jpos.org>
2002-01-25 01:53:14 +0000

    The classpath field in build.bat in jpos needs to be inquotes to allow for fodler names containing spaces.. (reported by rajal)

--- jpos/bin/build.bat
+++ jpos/bin/build.bat
@@ -5,5 +5,5 @@ set cp=%CLASSPATH%
 for %%i in (ext\*.jar) do call bin\cp.bat %%i
 for %%i in (lib\*.jar) do call bin\cp.bat %%i
 set CP=%JAVA_HOME%\lib\tools.jar;%CP%
-%JAVA% -classpath %CP% -Dant.home=lib org.apache.tools.ant.Main %1 %2 %3 %4 %5 %6 -buildfile src/build.xml
+%JAVA% -classpath "%CP%" -Dant.home=lib org.apache.tools.ant.Main %1 %2 %3 %4 %5 %6 -buildfile src/build.xml
 set CP=
