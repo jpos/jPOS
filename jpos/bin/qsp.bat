53b9b4c3ce672db4bf4f02cf743c12d15f298e9a
Hani S. Kirollos <h.samuel@egyptianbanks.net>
2002-02-26 18:12:00 +0000

    windows batch file to launch QSP from source code distro (without using jpos.jar), useful during development.

---
+++ jpos/bin/qsp.bat
@@ -0,0 +1,10 @@
+@echo off
+REM $Id$
+set JAVA=%JAVA_HOME%\bin\java
+set cp=build\classes;build\examples;lib\xerces_1_2_3.jar;lib\xalan_2_0_0.jar;lib\log4j.jar;lib\jmxri.jar%CLASSPATH%
+for %%i in (ext\*.jar) do call bin\cp.bat %%i
+set CP=%CP%;%JAVA_HOME%\lib\tools.jar
+
+%JAVA% -classpath %CP% org.jpos.apps.qsp.QSP %1
+
+set cp=
