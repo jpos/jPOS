adfb8776d9249972a31b25af6da4668f85f02c24
Alejandro Revilla <apr@jpos.org>
2002-09-18 00:19:30 +0000

    removed $Log - use cvs log instead

--- jpos/src/main/org/jpos/util/SimpleLogSource.java
+++ jpos/src/main/org/jpos/util/SimpleLogSource.java
@@ -49,31 +49,6 @@
 
 package org.jpos.util;
 
-/*
- * $Log$
- * Revision 1.2  2000/11/02 12:09:18  apr
- * Added license to every source file
- *
- * Revision 1.1  2000/04/16 23:53:14  apr
- * LogProducer renamed to LogSource
- *
- * Revision 1.2  2000/03/01 14:44:45  apr
- * Changed package name to org.jpos
- *
- * Revision 1.1  2000/01/11 01:25:02  apr
- * moved non ISO-8583 related classes from jpos.iso to jpos.util package
- * (AntiHog LeasedLineModem LogEvent LogListener LogSource
- *  Loggeable Logger Modem RotateLogListener SimpleAntiHog SimpleDialupModem
- *  SimpleLogListener SimpleLogSource SystemMonitor V24)
- *
- * Revision 1.2  1999/12/20 00:56:46  apr
- * Added public constructors
- *
- * Revision 1.1  1999/11/24 18:03:28  apr
- * added to repository
- *
- */
-
 /**
  * LogSources can choose to extends this SimpleLogSource
  *
