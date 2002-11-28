caa2a19191dfc76c5dbc21fe44c942fa94d8296d
Alejandro Revilla <apr@jpos.org>
2002-11-28 22:58:58 +0000

    ServerAdaptor example - a simple bouncer

---
+++ jpos/src/examples/q2/bouncer/Bouncer.java
@@ -0,0 +1,26 @@
+package bouncer;
+
+import org.jpos.iso.*;
+import org.jpos.util.*;
+
+public class Bouncer 
+    extends SimpleLogSource
+    implements ISORequestListener {
+    public boolean process (ISOSource source, ISOMsg m) 
+    {
+        try {
+            m = (ISOMsg) m.clone ();
+            m.setResponseMTI ();
+            m.set (39, "00");
+            m.set (38, 
+                ISOUtil.zeropad (
+                    Long.toString (System.currentTimeMillis () % 1000000), 6
+                )
+            );
+            source.send (m);
+        } catch (Exception e) {
+            Logger.log (new LogEvent (this, "warn", e));
+        }
+        return true;
+    }
+}
