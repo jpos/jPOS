37019ce643344cba2b0c1cddae6fdf5f765fe437
Alejandro Revilla <apr@jpos.org>
2002-11-29 00:02:21 +0000

    Updated example to run against Q2 server.

--- jpos/src/examples/remotespace/Test.java
+++ jpos/src/examples/remotespace/Test.java
@@ -2,57 +2,74 @@ package remotespace;
 
 import org.jpos.iso.*;
 import org.jpos.space.*;
+import org.jpos.util.Profiler;
 import javax.naming.*;
 
 /*
- * In order to run this example you should start QSP in another session
- * with the following configuration blocks:
- *
- * <channel name="xmlchannel_8001"
- *        class="org.jpos.iso.channel.XMLChannel"
- *        packager="org.jpos.iso.packager.XMLPackager"
- *        type="client" connect="no" logger="qsp" realm="xmlchannel_8001" >
- * <property name="host" value="127.0.0.1" />
- * <property name="port" value="8001" />
- *</channel>
- *
- * <task name="ISOChannelAdaptor" class="org.jpos.space.ISOChannelAdaptor">
- *  <property name="channel" value="xmlchannel_8001" />
- *  <property name="to"   value="xmlchannel_8001.tx" />
- *  <property name="from" value="xmlchannel_8001.rx" />
- * </task>
- *
- * <task name="SpaceProxy" class="org.jpos.space.SpaceProxy" 
- *     logger="qsp" realm="space-proxy" />
- *
- * Then you can use netcat with a command like this:
- * nc -v -p 8001 -l
- *
- * Every time you fire 'bin/example remotespace' you should read a message
- * like this on your nc console:
- *
- * <isomsg direction="outgoing">
- *   <field id="0" value="0800"/>
- *   <field id="3" value="000000"/>
- *   <field id="11" value="000000"/>
- *   <field id="41" value="29110001"/>
- * </isomsg>
- *
- * Change jndi.properties if you want to run this example on 
- * different machines.
- */
+   In order to run this example you should start Q2 with
+   the following files:
+ 
+10_bouncer.xml:
+
+<server class="org.jpos.q2.qbean.ServerAdaptor" 
+       logger="Q2" name="ExampleServer">
+ <attr name="port" type="java.lang.Integer">9000</attr>
+ <channel class="org.jpos.iso.channel.XMLChannel" 
+         logger="Q2" packager="org.jpos.iso.packager.XMLPackager">
+ </channel>
+ <request-listener class="bouncer.Bouncer" logger="Q2" realm="bouncer" />
+</server>
+
+20_channel.xml:
+
+<channel-adaptor class="org.jpos.q2.qbean.ChannelAdaptor" logger="Q2">
+ <!-- ISOChannel configuration -->
+ <channel class="org.jpos.iso.channel.XMLChannel" logger="Q2" 
+       packager="org.jpos.iso.packager.XMLPackager">
+  <property name="host" value="127.0.0.1" />
+  <property name="port" value="9000" />
+  <filter class="org.jpos.iso.filter.MacroFilter" direction="outgoing">
+   <property name="srcid" value="123456" />
+  </filter>
+ </channel>
+ <in>send</in>
+ <out>receive</out>
+ <reconnect-delay>1000</reconnect-delay>
+</channel-adaptor>
+
+30_spaceproxy.xml:
+
+<spaceproxy class="org.jpos.q2.qbean.SpaceProxyAdaptor" 
+       logger="Q2" name="SpaceProxy" />
+
+  
+NOTE: Change jndi.properties if you want to connect to a remote
+      server.
+
+*/
 
 public class Test {
     public static void main (String[] args) {
+        Profiler prof = new Profiler ();
         try {
             InitialContext ctx = new InitialContext ();
             RemoteSpace sp = (RemoteSpace) ctx.lookup ("SpaceProxy");
+            prof.checkPoint ("get-proxy");
             ISOMsg m = new ISOMsg();
             m.setMTI ("0800");
             m.set (3, "000000");
             m.set (11, "000000");
             m.set (41, "29110001");
-            sp.out ("xmlchannel_8001.tx", m);
+            sp.out ("send", m);
+            prof.checkPoint ("send");
+            m.dump (System.out, "--> ");
+
+            ISOMsg r = (ISOMsg) sp.in ("receive");
+            prof.checkPoint ("receive");
+            r.dump (System.out, "<-- ");
+
+            prof.dump (System.out, "");
+
         } catch (Exception e) {
             e.printStackTrace ();
         }
