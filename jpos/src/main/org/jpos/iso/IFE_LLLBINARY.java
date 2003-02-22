e7014796f0069f8e22d4c7232a5e9abe30b7422b
Alejandro Revilla <apr@jpos.org>
2003-02-22 21:51:27 +0000

    EBCDIC version of IFB_LLLBINARY

---
+++ jpos/src/main/org/jpos/iso/IFE_LLLBINARY.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) 2000 jPOS.org.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *    "This product includes software developed by the jPOS project 
+ *    (http://www.jpos.org/)". Alternately, this acknowledgment may 
+ *    appear in the software itself, if and wherever such third-party 
+ *    acknowledgments normally appear.
+ *
+ * 4. The names "jPOS" and "jPOS.org" must not be used to endorse 
+ *    or promote products derived from this software without prior 
+ *    written permission. For written permission, please contact 
+ *    license@jpos.org.
+ *
+ * 5. Products derived from this software may not be called "jPOS",
+ *    nor may "jPOS" appear in their name, without prior written
+ *    permission of the jPOS project.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  
+ * IN NO EVENT SHALL THE JPOS PROJECT OR ITS CONTRIBUTORS BE LIABLE FOR 
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS 
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING 
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the jPOS Project.  For more
+ * information please see <http://www.jpos.org/>.
+ */
+
+package org.jpos.iso;
+import java.io.InputStream;
+import java.io.IOException;
+
+/**
+ * BINARY version of IFE_LLLCHAR
+ * Uses a 3 EBCDIC byte length field
+ * 
+ * @author Alejandro Revila
+ * @version $Id$
+ * @see ISOFieldPackager
+ * @see ISOComponent
+ */
+public class IFE_LLLBINARY extends ISOFieldPackager 
+{
+    public IFE_LLLBINARY()
+    {
+	super();
+    }
+    /**
+    * @param len - field len
+    * @param description symbolic descrption
+    */
+    public IFE_LLLBINARY(int len, String description) 
+    {
+	super(len, description);
+    }
+    /**
+    * @param c - a component
+    * @return packed component
+    * @exception ISOException
+    */
+    public byte[] pack(ISOComponent c) throws ISOException 
+    {
+        byte[] v = (byte[]) c.getValue();
+        int len  = v.length;
+        if (len > getLength() || len>999)
+            throw new ISOException (
+                "invalid len "+len +" packing field "+ (Integer) c.getKey()
+            );
+
+	String s = new String (v);
+	String l = ISOUtil.zeropad(Integer.toString(len), 3);
+	return ISOUtil.asciiToEbcdic(l + s);
+    }
+    /**
+    * @param c - the Component to unpack
+    * @param b - binary image
+    * @param offset - starting offset within the binary image
+    * @return consumed bytes
+    * @exception ISOException
+    */
+    public int unpack(ISOComponent c, byte[] b, int offset)
+	throws ISOException
+    {
+        int len = Integer.parseInt (ISOUtil.ebcdicToAscii(b, offset, 3));
+	c.setValue(ISOUtil.ebcdicToAsciiBytes(b, offset+3, len));
+	return len+3;
+    }
+    public int getMaxPackedLength() 
+    {
+	return getLength()+3;
+    }
+    public ISOComponent createComponent(int fieldNumber) {
+        return new ISOBinaryField (fieldNumber);
+    }
+    public void unpack (ISOComponent c, InputStream in) 
+        throws IOException, ISOException
+    {
+        int len = Integer.parseInt (ISOUtil.ebcdicToAscii(readBytes (in, 3)));
+	c.setValue(ISOUtil.ebcdicToAscii(readBytes (in, len)));
+    }
+}
