46331ad11bf568de15f6b2a07e2da34c324abe3a
Alejandro Revilla <apr@jpos.org>
2003-05-22 18:31:58 +0000

    getAvailableCount() before accepting new connections

--- jpos/src/main/org/jpos/iso/ISOServer.java
+++ jpos/src/main/org/jpos/iso/ISOServer.java
@@ -295,7 +295,7 @@ public class ISOServer extends Observable
 			setFilters (channel);
 			if (channel instanceof Observable)
 			    ((Observable)channel).addObserver (this);
-                        while (pool.getIdleCount() <= 0)
+                        while (pool.getAvailableCount() <= 0)
                             ISOUtil.sleep (10);
 			channel.accept (serverSocket);
                         if ((cnt[CONNECT]++) % 100 == 0)
