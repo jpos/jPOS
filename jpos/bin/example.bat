d956f1806205aba920e52aec44a2035239f706ab
Alejandro Revilla <apr@jpos.org>
2003-09-02 01:43:04 +0000

    Added JDom jars

--- jpos/bin/example.bat
+++ jpos/bin/example.bat
@@ -1,7 +1,7 @@
 @echo off
 REM $Id$
 set JAVA=%JAVA_HOME%\bin\java
-set cp=build\classes;lib\xerces_1_2_3.jar;lib\xalan_2_0_0.jar;lib\log4j.jar;lib/mx4j-jmx.jar;lib/mx4j-tools.jar;%CLASSPATH%
+set cp=build\classes;lib\jdom.jar;lib\xerces_1_2_3.jar;lib\xalan_2_0_0.jar;lib\log4j.jar;lib/mx4j-jmx.jar;lib/mx4j-tools.jar;%CLASSPATH%
 for %%i in (ext\*.jar) do call bin\cp.bat %%i
 set CP=%CP%;%JAVA_HOME%\lib\tools.jar
 
