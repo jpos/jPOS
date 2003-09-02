47234c30e217fcf5f3be9d41c7ea1b65fe3e9f9f
Alejandro Revilla <apr@jpos.org>
2003-09-02 11:35:56 +0000

    A simple action that exits the application

---
+++ jpos/src/main/org/jpos/ui/action/Exit.java
@@ -0,0 +1,23 @@
+package org.jpos.ui.action;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import org.jpos.ui.UI;
+import org.jpos.ui.UIAware;
+import org.jdom.Element;
+
+public class Exit implements ActionListener, UIAware {
+    public UI ui;
+    public Exit () {
+        super();
+    }
+    public void setUI (UI ui, Element e) {
+        this.ui = ui;
+    }
+    public void actionPerformed (ActionEvent ev) {
+        ui.dispose ();
+        System.exit (0);
+    }
+}
+
