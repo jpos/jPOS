4d0312522782b4860ecd8de06fc41bf0e4c60630
Andres Alcarraz <alcarraz@users.sourceforge.net>
2003-10-28 17:40:54 +0000

    Added support for:
    1 - preload-scripts, now you can set BSHLogListener to maintain in memory unmodified scripts
     this is usefull when your scripts are not too big to avoid accessing the hard drive for each event.
    Note: the disk will be accessed to verifiy if the script was touched.
    
    2 - filter-by-default, if this option is set to true and there's no script to handle a given log event
    the log method will return true so that Logger stop processing that event.
    
    both attributes are set by the setConfiguration method.

--- jpos/src/ext/org/jpos/bsh/BSHLogListener.java
+++ jpos/src/ext/org/jpos/bsh/BSHLogListener.java
@@ -49,8 +49,14 @@
 
 package org.jpos.bsh;
 
+import bsh.EvalError;
 import bsh.Interpreter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.Hashtable;
+import java.util.Map;
 import org.jpos.core.Configuration;
 import org.jpos.util.LogEvent;
 
@@ -78,18 +84,34 @@ import org.jpos.util.LogEvent;
  *       <font color="#008080">&lt;</font><font color="#008080">property</font><font color="#008080"> </font><font color="#2e8b57"><b>name</b></font>=<font color="#ff00ff">&quot;source&quot;</font><font color="#008080"> </font><font color="#2e8b57"><b>value</b></font>=<font color="#ff00ff">&quot;tag_$tag_realm_$realm.bsh&quot;</font><font color="#008080">/&gt;</font> <font color="#0000ff">&lt;!</font><font color="#0000ff">-- to handle specific tags from specific realms--</font><font color="#0000ff">&gt;</font>
  * <font color="#008080">&lt;/log-listener&gt;</font>
  * </pre>
- * If a source with the given name is not found, it is not processed, so this lets
+ * <!-- end of lines for html doc-->
+ * If a source with the given name is not found, or it canbe read, it is not processed, so this lets
  * you change what is processed in real time, if you put a file called
  * tag_SystemMonitor_realm_monitor.bsh it will be executed whenever the system
  * monitor is run.<BR>
- * <B><I>Important: </I></B> all scripts must return an event, if not it is
- * consider that the intention of the script is to filter that message.
- * @author <a href="alcarraz@fing.edu.uy"> Andrés Alcarraz </a>
+ * If you want to filter an event so that the remaining log listeners doesn't see
+ * it, you have to set event = null in your script.<br>
+ * <table border=1 color="black">
+ * <caption>Other Configuration Options: </caption>
+ * <th><td> Name                </td><td>Type       </td><td>Description</td></th>
+ * <tr><td> filter-by-default   </td><td>boolean    </td><td>
+ *  If true, filter the events if no script is processed, this way you can put a
+ * Log Listener that filters every thing unless you put some script file, even if
+ * it is empty, you also can dynamically change what kind of message are filtered
+ * by placing empty files with names like tag_SystemMonitor_realm_monitor.bsh in
+ * the previous example, if you "touch" a file with this name these events will
+ * begin to be processed.</TD></TR>
+ * <TR><TD> preload-scripts     </TD><TD>boolean    </TD><TD>If true scripts a
+ * loaded once, and kept in memory, being realoaded only if they are touched. This
+ * is good when you have lots of RAM memory but ou have troubles with
+ * speed</TD></TR>
+ * </TABLE>
  */
 public class BSHLogListener implements org.jpos.util.LogListener, org.jpos.core.Configurable {
     /**Holds the configuration for this object*/
     protected Configuration cfg;
     protected static final String[] patterns = {"tag", "realm"};
+    protected Map scripts = new Hashtable();
     /** Creates a new instance of BSHLogListener */
     public BSHLogListener() {
     }
@@ -119,30 +141,102 @@ public class BSHLogListener implements org.jpos.util.LogListener, org.jpos.core.
     }
     public LogEvent log(org.jpos.util.LogEvent ev) {
         LogEvent ret = ev;
-        //boolean processed = false;
+        boolean processed = false;
         try{
             String[] sources = replace(cfg.getAll("source"), patterns, new String[] {ev.tag, ev.getRealm()});
             for(int i=0; i<sources.length && ret != null; i++){
                 try{
                     File f = new File(sources[i]);
                     if(f.exists() && f.canRead() && f.isFile()){
+                        //if(f.lastModified())
+                        processed = true;
                         Interpreter bsh = new Interpreter();
                         bsh.set("event", ret);
                         bsh.set("cfg", cfg);
-                        bsh.eval(new java.io.FileReader(f));
+                        if(!cfg.getBoolean("preload-scripts"))
+                            bsh.eval(new java.io.FileReader(f));
+                        else
+                            eval(bsh, f, sources[i]);
                         ret = (LogEvent)bsh.get("event");
-                    }
+                    }else if(cfg.getBoolean("preload-scripts")) 
+                        scripts.remove(sources[i]);
                 }catch(Exception e){
                     ret.addMessage(e);
                 }
             }
-            //if(!processed) return cfg.getBoolean("filter-by-default")?null:ev;
+            if(!processed && cfg.getBoolean("filter-by-default")) return null;
             return ret;
         }catch(Exception e){
             ret.addMessage(e);
             return ret;
         }
     }
+    protected String loadCode(File f) throws IOException{
+        FileReader r = new FileReader(f);
+        StringBuffer buf = new StringBuffer((int)f.length());
+        char[] content = new char[(int)f.length()];
+        int l;
+        while((l=r.read(content))!=-1) buf.append(content,0,l);
+        return buf.toString();
+    }
     
-    
+    protected void eval(Interpreter bsh, File f, String source) throws EvalError, IOException{
+        ScriptInfo info = (ScriptInfo)scripts.get(source);
+        if(info == null) scripts.put(source, info=new ScriptInfo());
+        String code;
+        if(info.getLastModified() != f.lastModified()) {
+            info.setLastModified(f.lastModified());
+            info.setCode(loadCode(f));
+        }
+        bsh.eval(new StringReader(info.getCode()));
+    }
+    protected ScriptInfo getScriptInfo(String filename){
+        return (ScriptInfo)scripts.get(filename);
+    }
+    protected void addScriptInfo(String filename, String code, long lastModified){
+        scripts.put(filename, new ScriptInfo(code, lastModified));
+    }
+    protected class ScriptInfo{
+        String code;
+        long lastModified;
+        public ScriptInfo(){
+        }
+        public ScriptInfo(String code, long lastModified){
+            setCode(code);
+            setLastModified(lastModified);
+        }
+        
+        /** Getter for property code.
+         * @return Value of property code.
+         *
+         */
+        public java.lang.String getCode() {
+            return code;
+        }
+        
+        /** Setter for property code.
+         * @param code New value of property code.
+         *
+         */
+        public void setCode(java.lang.String code) {
+            this.code = code;
+        }
+        
+        /** Getter for property lastModified.
+         * @return Value of property lastModified.
+         *
+         */
+        public long getLastModified() {
+            return lastModified;
+        }
+        
+        /** Setter for property lastModified.
+         * @param lastModified New value of property lastModified.
+         *
+         */
+        public void setLastModified(long lastModified) {
+            this.lastModified = lastModified;
+        }
+        
+    }
 }
