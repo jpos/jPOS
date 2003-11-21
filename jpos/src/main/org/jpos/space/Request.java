b9cd94f586283a245dfec09805c4cd5d898094d7
Alejandro Revilla <apr@jpos.org>
2003-11-21 12:38:54 +0000

    Helper class used for space based client/server communications

---
+++ jpos/src/main/org/jpos/space/Request.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2003 jPOS.org
+ *
+ * See terms of license at http://jpos.org/license.html
+ *
+ */
+package org.jpos.space;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Request adds to TinySpace a few helper methods suitable for
+ * exchanging requests and responses over an outter [Transient|Tiny]Space.
+ *
+ * @author Alejandro Revilla
+ * @version $Revision$ $Date$
+ * @since 1.4.7
+ */
+public class Request extends TinySpace {
+    public static String REQUEST  = "$REQ";
+    public static String RESPONSE = "$RESP";
+    public Request () {
+        super();
+    }
+    public Request (Object value) {
+        out (REQUEST, value);
+    }
+    public Object getResponse () {
+        return in (RESPONSE);
+    }
+    public Object getResponse (long timeout) {
+        return in (RESPONSE, timeout);
+    }
+}
+
