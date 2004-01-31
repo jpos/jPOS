803bf455622aacc1d97d994b356b4e87b8841ad3
Luar Roji <luar@roji.net>
2004-01-31 03:07:58 +0000

    Now implements UIAware.

--- jpos/src/main/org/jpos/ui/action/BSH.java
+++ jpos/src/main/org/jpos/ui/action/BSH.java
@@ -4,12 +4,21 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import bsh.Interpreter;
 
-public class BSH implements ActionListener {
+import org.jpos.ui.UI;
+import org.jpos.ui.UIAware;
+import org.jdom.Element;
+
+public class BSH implements ActionListener, UIAware {
+    public UI ui;
 
     public BSH() {
         super();
     }
-    
+
+    public void setUI (UI ui, Element e) {
+        this.ui = ui;
+    }
+
     public void actionPerformed (ActionEvent ev) {
         String bshSource = ev.getActionCommand();
         try {
