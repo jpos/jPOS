4e52eb654585732fed2b0ea3591ea9742ee855d3
Alejandro Revilla <apr@jpos.org>
2004-04-10 17:26:11 +0000

    Upgrade MX4J to 2.0.1

--- q2/src/main/org/jpos/q2/qbean/HttpAdaptor.java
+++ q2/src/main/org/jpos/q2/qbean/HttpAdaptor.java
@@ -1,50 +1,8 @@
 /*
- * Copyright (c) 2000 jPOS.org.  All rights reserved.
+ * Copyright (c) 2004 jPOS.org 
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ * See terms of license at http://jpos.org/license.html
  *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:
- *    "This product includes software developed by the jPOS project
- *    (http://www.jpos.org/)". Alternately, this acknowledgment may
- *    appear in the software itself, if and wherever such third-party
- *    acknowledgments normally appear.
- *
- * 4. The names "jPOS" and "jPOS.org" must not be used to endorse
- *    or promote products derived from this software without prior
- *    written permission. For written permission, please contact
- *    license@jpos.org.
- *
- * 5. Products derived from this software may not be called "jPOS",
- *    nor may "jPOS" appear in their name, without prior written
- *    permission of the jPOS project.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE JPOS PROJECT OR ITS CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
- * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the jPOS Project.  For more
- * information please see <http://www.jpos.org/>.
  */
 package org.jpos.q2.qbean;
 
@@ -60,9 +18,9 @@ import javax.management.*;
 
 /**
  * @author <a href="mailto:taherkordy@dpi2.dpi.net.ir">Alireza Taherkordi</a>
+ * @author Alejandro Revilla
  *
- * <http-adaptor class="org.jpos.q2.qbean.HttpAdaptor"
- *                       name="http-adaptor" >
+ * <http-adaptor class="org.jpos.q2.qbean.HttpAdaptor" name="http-adaptor" >
  *  <attr name="host">localhost</attr>
  *  <attr name="port" type="java.lang.Integer">8082</attr>
  * </http-adaptor>
@@ -73,7 +31,7 @@ import javax.management.*;
  * @version $Revision$ $Date$
  */
 public class HttpAdaptor
-            extends mx4j.adaptor.http.HttpAdaptor
+            extends mx4j.tools.adaptor.http.HttpAdaptor
         implements  HttpAdaptorMBean , QBean, QPersist
 {
     Element persist;
@@ -127,8 +85,8 @@ public class HttpAdaptor
             return;
         try {
             setModified (false);
-            processorName = new ObjectName("MX4J:name=mx4j.adaptor.http.XSLTProcessor");
-            mx4j.adaptor.http.XSLTProcessor processorMBean = new mx4j.adaptor.http.XSLTProcessor();
+            processorName = new ObjectName("MX4J:name=mx4j.tools.adaptor.http.XSLTProcessor");
+            mx4j.tools.adaptor.http.XSLTProcessor processorMBean = new mx4j.tools.adaptor.http.XSLTProcessor();
             getServer().getMBeanServer().registerMBean(processorMBean,processorName);
             setProcessorName(processorName);
             if (user != null && password != null) {
--- q2/src/main/org/jpos/q2/qbean/HttpAdaptorMBean.java
+++ q2/src/main/org/jpos/q2/qbean/HttpAdaptorMBean.java
@@ -1,50 +1,8 @@
 /*
- * Copyright (c) 2000 jPOS.org.  All rights reserved.
+ * Copyright (c) 2004 jPOS.org 
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ * See terms of license at http://jpos.org/license.html
  *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:
- *    "This product includes software developed by the jPOS project
- *    (http://www.jpos.org/)". Alternately, this acknowledgment may
- *    appear in the software itself, if and wherever such third-party
- *    acknowledgments normally appear.
- *
- * 4. The names "jPOS" and "jPOS.org" must not be used to endorse
- *    or promote products derived from this software without prior
- *    written permission. For written permission, please contact
- *    license@jpos.org.
- *
- * 5. Products derived from this software may not be called "jPOS",
- *    nor may "jPOS" appear in their name, without prior written
- *    permission of the jPOS project.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE JPOS PROJECT OR ITS CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
- * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the jPOS Project.  For more
- * information please see <http://www.jpos.org/>.
  */
 package org.jpos.q2.qbean;
 
@@ -55,7 +13,7 @@ import org.jpos.q2.QBeanSupportMBean;
  * @version $Revision$ $Date$
  */
 public interface HttpAdaptorMBean
-        extends mx4j.adaptor.http.HttpAdaptorMBean,QBeanSupportMBean
+        extends mx4j.tools.adaptor.http.HttpAdaptorMBean,QBeanSupportMBean
 {
     public void setUser (String user);
     public String getUser ();
