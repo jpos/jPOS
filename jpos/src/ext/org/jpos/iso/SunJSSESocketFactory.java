9fbaeceb9b9eaac558f175686bc2a56b4beaf414
Alwyn Schoeman <alwyn.schoeman@gmail.com>
2004-04-27 15:02:27 +0000

    Made Reconfigurable. Added setConfiguration for setting keystore and client authentication. Added client authentication. In order to use add the following to the deployment descriptor:   <property name="keystore" value="/path/to/keystore"/>   <property name="clientauth" value="true"/>

--- jpos/src/ext/org/jpos/iso/SunJSSESocketFactory.java
+++ jpos/src/ext/org/jpos/iso/SunJSSESocketFactory.java
@@ -1,50 +1,8 @@
 /*
- * Copyright (c) 2000 jPOS.org.  All rights reserved.
+ * Copyright (c) 2004 jPOS.org 
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ * See terms of license at http://jpos.org/license.html
  *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:
- *    "This product includes software developed by the jPOS project 
- *    (http://www.jpos.org/)". Alternately, this acknowledgment may 
- *    appear in the software itself, if and wherever such third-party 
- *    acknowledgments normally appear.
- *
- * 4. The names "jPOS" and "jPOS.org" must not be used to endorse 
- *    or promote products derived from this software without prior 
- *    written permission. For written permission, please contact 
- *    license@jpos.org.
- *
- * 5. Products derived from this software may not be called "jPOS",
- *    nor may "jPOS" appear in their name, without prior written
- *    permission of the jPOS project.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  
- * IN NO EVENT SHALL THE JPOS PROJECT OR ITS CONTRIBUTORS BE LIABLE FOR 
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS 
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING 
- * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
- * POSSIBILITY OF SUCH DAMAGE.
- * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the jPOS Project.  For more
- * information please see <http://www.jpos.org/>.
  */
 
 package org.jpos.iso;
@@ -61,11 +19,15 @@ import java.security.Security;
 import javax.net.ssl.KeyManager;
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLServerSocketFactory;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
 
+import org.jpos.core.Configuration;
+import org.jpos.core.ConfigurationException;
+import org.jpos.core.ReConfigurable;
 import org.jpos.util.SimpleLogSource;
 
 /**
@@ -74,11 +36,12 @@ import org.jpos.util.SimpleLogSource;
  *
  * @version $Revision$ $Date$
  * @author  Bharavi Gade
+ * @author Alwyn Schoeman
  * @since   1.3.3
  */
 public class SunJSSESocketFactory 
         extends SimpleLogSource 
-        implements ISOServerSocketFactory,ISOClientSocketFactory
+        implements ISOServerSocketFactory,ISOClientSocketFactory,ReConfigurable
 { 
 
     private SSLContext sslc=null;
@@ -88,6 +51,8 @@ public class SunJSSESocketFactory
     private String keyStore=null;
     private String password=null;
     private String keyPassword=null;
+    private boolean clientAuthNeeded=false;
+    private Configuration cfg;
 
     public void setKeyStore(String keyStore){
         this.keyStore=keyStore;  
@@ -179,7 +144,9 @@ public class SunJSSESocketFactory
         throws IOException, ISOException
     {
         if(serverFactory==null) serverFactory=createServerSocketFactory();
-        return serverFactory.createServerSocket(port);
+        ServerSocket socket = serverFactory.createServerSocket(port);
+        ((SSLServerSocket) socket).setNeedClientAuth(clientAuthNeeded);
+        return socket;
     }
     
    /**
@@ -208,5 +175,14 @@ public class SunJSSESocketFactory
     protected String getKeyPassword()
     {
         return "password";
+    }
+
+    /**
+     * @see org.jpos.core.Configurable#setConfiguration(org.jpos.core.Configuration)
+     */
+    public void setConfiguration(Configuration cfg) throws ConfigurationException {
+        this.cfg = cfg;
+        keyStore = cfg.get("keystore");
+        clientAuthNeeded = cfg.getBoolean("clientauth");
     }
 }
