dcc92d9d8d644784290fb21006a752d828bf1ddc
Alwyn Schoeman <alwyn.schoeman@gmail.com>
2004-05-04 16:55:13 +0000

    Added getLoader and getLoaderURLS method to the Q2 mbean. These provide classloader information about Q2's loader.

--- q2/src/main/org/jpos/q2/QBeanSupport.java
+++ q2/src/main/org/jpos/q2/QBeanSupport.java
@@ -1,65 +1,23 @@
 /*
- * Copyright (c) 2000 jPOS.org.  All rights reserved.
+ * Copyright (c) 2004 jPOS.org 
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ * See terms of license at http://jpos.org/license.html
  *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:
- *    "This product includes software developed by the jPOS project
- *    (http://www.jpos.org/)". Alternately, this acknowledgment may
- *    appear in the software itself, if and wherever such third-party
- *    acknowledgments normally appear.
- *
- * 4. The names "jPOS" and "jPOS.org" must not be used to endorse
- *    or promote products derived from this software without prior
- *    written permission. For written permission, please contact
- *    license@jpos.org.
- *
- * 5. Products derived from this software may not be called "jPOS",
- *    nor may "jPOS" appear in their name, without prior written
- *    permission of the jPOS project.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE JPOS PROJECT OR ITS CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
- * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the jPOS Project.  For more
- * information please see <http://www.jpos.org/>.
  */
+
 package org.jpos.q2;
 
 import org.jdom.Element;
 import org.jpos.util.Log;
-import org.jpos.util.Logger;
 import org.jpos.core.Configurable;
 import org.jpos.core.Configuration;
 import org.jpos.core.ConfigurationException;
 
 import java.util.Iterator;
 import java.lang.reflect.Method;
+import java.net.URL;
 import java.beans.BeanInfo;
 import java.beans.Introspector;
-import java.beans.IntrospectionException;
 import java.beans.PropertyDescriptor;
 
 /**
@@ -176,6 +134,12 @@ public class QBeanSupport
     public int getState () {
         return state;
     }
+    public URL[] getLoaderURLS() {
+        return server.getLoader().getURLs();
+    }    
+    public QClassLoader getLoader() {
+        return server.getLoader();
+    }
     public String getStateAsString () {
         return state >= 0 ? stateString[state] : "Unknown";
     }
@@ -291,5 +255,6 @@ public class QBeanSupport
     public Configuration getConfiguration () {
         return cfg;
     }
+
 }
 
