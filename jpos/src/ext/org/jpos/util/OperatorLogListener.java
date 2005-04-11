33caf18b3b4c101631f385b9a2e557c71651c6e0
Alejandro Revilla <apr@jpos.org>
2005-04-11 15:58:24 +0000

    If jpos.operator.tags has 'exception' in it, we log any LogEvent that carries a Throwable payload.

--- jpos/src/ext/org/jpos/util/OperatorLogListener.java
+++ jpos/src/ext/org/jpos/util/OperatorLogListener.java
@@ -1,90 +1,7 @@
 /*
- * Copyright (c) 2000 jPOS.org.  All rights reserved.
+ * Copyright (c) 2005 jPOS.org 
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:
- *    "This product includes software developed by the jPOS project 
- *    (http://www.jpos.org/)". Alternately, this acknowledgment may 
- *    appear in the software itself, if and wherever such third-party 
- *    acknowledgments normally appear.
- *
- * 4. The names "jPOS" and "jPOS.org" must not be used to endorse 
- *    or promote products derived from this software without prior 
- *    written permission. For written permission, please contact 
- *    license@jpos.org.
- *
- * 5. Products derived from this software may not be called "jPOS",
- *    nor may "jPOS" appear in their name, without prior written
- *    permission of the jPOS project.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  
- * IN NO EVENT SHALL THE JPOS PROJECT OR ITS CONTRIBUTORS BE LIABLE FOR 
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS 
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING 
- * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
- * POSSIBILITY OF SUCH DAMAGE.
- * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the jPOS Project.  For more
- * information please see <http://www.jpos.org/>.
- */
-
-/*
- * $Log$
- * Revision 1.10  2003/10/13 11:04:20  apr
- * tabs expanded to spaces
- *
- * Revision 1.9  2003/09/03 00:22:14  apr
- * New LogListener interface returns [possibly null] LogEvent
- *
- * Revision 1.8  2003/05/21 17:57:34  apr
- * Supports multiple destinations:
- *
- *   "jpos.operator.to"
- *   "jpos.operator.cc"
- *   "jpos.operator.bcc"
- *
- * Revision 1.7  2003/05/16 07:14:30  alwyns
- * Import cleanups. Should work as expected now.
- *
- * Revision 1.6  2002/12/04 11:34:12  apr
- * Oops: missing import
- *
- * Revision 1.5  2002/12/04 01:32:43  apr
- * Different filenames in multiparts.
- * End filenames with ".txt" so dumb mailers that doesn't
- * understand text/plain mimetype give you a chance to open the
- * attachment without saving it to disk.
- *
- * Revision 1.4  2000/11/02 12:09:17  apr
- * Added license to every source file
- *
- * Revision 1.3  2000/05/25 23:34:13  apr
- * Implements Configurable (used by QSP)
- *
- * Revision 1.2  2000/03/01 14:44:45  apr
- * Changed package name to org.jpos
- *
- * Revision 1.1  2000/01/23 16:04:48  apr
- * Added OperatorLogListener
+ * See terms of license at http://jpos.org/license.html
  *
  */
 
@@ -119,6 +36,7 @@ public class OperatorLogListener
 {
     Configuration cfg;
     BlockingQueue queue;
+    boolean logExceptions;
 
     public OperatorLogListener () {
         super();
@@ -139,6 +57,10 @@ public class OperatorLogListener
         assertProperty ("jpos.operator.tags");
         assertProperty ("jpos.operator.delay");
         assertProperty ("jpos.mail.smtp.host");
+
+        logExceptions = cfg.get ("jpos.operator.tags").
+            indexOf ("exception") >= 0;
+
         new Thread(this).start();
     }
     public void run() {
@@ -220,7 +142,16 @@ public class OperatorLogListener
     }
     private boolean checkOperatorTag(LogEvent ev) {
         String tags = cfg.get ("jpos.operator.tags");
-        return tags.indexOf (ev.tag) >= 0;
+
+        return (tags.indexOf (ev.tag) >= 0) || (logExceptions && hasException (ev));
+    }
+    private boolean hasException (LogEvent evt) {
+        Iterator iter = evt.getPayLoad().iterator();
+        while (iter.hasNext()) {
+            if (iter.next() instanceof Throwable)
+                return true;
+        }
+        return false;
     }
     private InternetAddress[] getAddress (String[] s) throws AddressException {
         InternetAddress[] address = new InternetAddress[s.length];
