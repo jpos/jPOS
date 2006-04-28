933bd1500241d47b0479e82005d1449f9f0112c1
Alejandro Revilla <apr@jpos.org>
2006-04-28 02:09:04 +0000

    Let's start testing the new DailyLogListener written by Andres

--- jpos6/modules/jpos/src/org/jpos/util/RotateLogListener.java
+++ jpos6/modules/jpos/src/org/jpos/util/RotateLogListener.java
@@ -1,52 +1,9 @@
 /*
- * Copyright (c) 2000 jPOS.org.  All rights reserved.
+ * Copyright (c) 2006 jPOS.org 
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ * See terms of license at http://jpos.org/license.html
  *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:
- *    "This product includes software developed by the jPOS project 
- *    (http://www.jpos.org/)". Alternately, this acknowledgment may 
- *    appear in the software itself, if and wherever such third-party 
- *    acknowledgments normally appear.
- *
- * 4. The names "jPOS" and "jPOS.org" must not be used to endorse 
- *    or promote products derived from this software without prior 
- *    written permission. For written permission, please contact 
- *    license@jpos.org.
- *
- * 5. Products derived from this software may not be called "jPOS",
- *    nor may "jPOS" appear in their name, without prior written
- *    permission of the jPOS project.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  
- * IN NO EVENT SHALL THE JPOS PROJECT OR ITS CONTRIBUTORS BE LIABLE FOR 
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS 
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING 
- * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
- * POSSIBILITY OF SUCH DAMAGE.
- * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the jPOS Project.  For more
- * information please see <http://www.jpos.org/>.
  */
-
 package org.jpos.util;
 
 import java.io.File;
@@ -155,13 +112,13 @@ public class RotateLogListener extends SimpleLogListener
         
         return super.log (ev);
     }
-    private synchronized void openLogFile() throws IOException {
+    protected synchronized void openLogFile() throws IOException {
         if (f != null)
             f.close();
         f = new FileOutputStream (logName, true);
         setPrintStream (new PrintStream(f));
     }
-    private synchronized void closeLogFile() throws IOException {
+    protected synchronized void closeLogFile() throws IOException {
         if (f != null)
             f.close();
         f = null;
@@ -187,7 +144,7 @@ public class RotateLogListener extends SimpleLogListener
             p.println ("</log>");
         }
     }
-    private void checkSize() {
+    protected void checkSize() {
         File logFile = new File (logName);
         if (logFile.length() > maxSize) {
             try {
