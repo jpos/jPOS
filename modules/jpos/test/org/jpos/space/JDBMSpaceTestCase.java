d51b47e23794c5b3b7c2ae51e94813f21232fdd6
Alejandro Revilla <apr@jpos.org>
2006-05-02 11:48:32 +0000

    added TSpace and JDBMSpace tests

---
+++ modules/jpos/test/org/jpos/space/JDBMSpaceTestCase.java
@@ -0,0 +1,113 @@
+package org.jpos.space;
+
+import junit.framework.*;
+import java.io.*;
+import java.util.*;
+import org.jpos.util.*;
+
+public class JDBMSpaceTestCase extends TestCase {
+    public static final int COUNT = 1000;
+    JDBMSpace sp;
+    public void setUp () {
+        sp = JDBMSpace.getSpace ("space-test");
+    }
+    public void testSimpleOut() throws Exception {
+        Object o = new Boolean (true);
+        sp.out ("testSimpleOut_Key", o);
+        Object o1 = sp.in ("testSimpleOut_Key");
+        assertTrue (o.equals (o1));
+    }
+    public void testOutRdpInpRdp() throws Exception {
+        Object o = new Boolean (true);
+        String k = "testOutRdpInpRdp_Key";
+        sp.out (k, o);
+        assertTrue (o.equals (sp.rdp (k)));
+        assertTrue (o.equals (sp.rd  (k)));
+        assertTrue (o.equals (sp.rd  (k, 1000)));
+        assertTrue (o.equals (sp.inp (k)));
+        assertTrue (sp.rdp (k) == null);
+        assertTrue (sp.rd  (k, 100) == null);
+    }
+    public void testMultiKeyLoad() throws Exception {
+        String s = "The quick brown fox jumped over the lazy dog";
+        Profiler prof = new Profiler ();
+        for (int i=0; i<COUNT; i++) {
+            sp.out ("testMultiKeyLoad_Key" + Integer.toString (i), s);
+            if (i % 100 == 0)
+                prof.checkPoint ("out " + i);
+        }
+        // prof.dump (System.err, "MultiKeyLoad out >");
+        prof = new Profiler ();
+        for (int i=0; i<COUNT; i++) {
+            assertTrue (s.equals (sp.in ("testMultiKeyLoad_Key" + Integer.toString (i))));
+            if (i % 100 == 0)
+                prof.checkPoint ("in " + i);
+        }
+        // prof.dump (System.err, "MultiKeyLoad in  >");
+    }
+    public void testNoAutoCommit () throws Exception {
+        String s = "The quick brown fox jumped over the lazy dog";
+        Profiler prof = new Profiler ();
+        synchronized (sp) {
+            sp.setAutoCommit (false);
+            for (int i=0; i<COUNT; i++) {
+                sp.out ("testNoAutoCommit_Key" + Integer.toString (i), s);
+                if (i % 100 == 0)
+                    prof.checkPoint ("out " + i);
+            }
+            prof.checkPoint ("pre-commit");
+            sp.commit();
+            sp.setAutoCommit (true);
+            prof.checkPoint ("commit");
+        }
+        // prof.dump (System.err, "NoAutoCommit out >");
+        prof = new Profiler ();
+        synchronized (sp) {
+            sp.setAutoCommit (false);
+            for (int i=0; i<COUNT; i++) {
+                assertTrue (s.equals (sp.in ("testNoAutoCommit_Key" + Integer.toString (i))));
+                if (i % 100 == 0)
+                    prof.checkPoint ("in " + i);
+            }
+            prof.checkPoint ("pre-commit");
+            sp.commit();
+            sp.setAutoCommit (true);
+            prof.checkPoint ("commit");
+        }
+        // prof.dump (System.err, "NoAutoCommit in  >");
+    }
+    public void testSingleKeyLoad() throws Exception {
+        String s = "The quick brown fox jumped over the lazy dog";
+        String k = "testSingleKeyLoad_Key";
+        Profiler prof = new Profiler ();
+        for (int i=0; i<COUNT; i++) {
+            sp.out (k, s);
+            if (i % 100 == 0)
+                prof.checkPoint ("out " + i);
+        }
+        // prof.dump (System.err, "SingleKeyLoad out >");
+        prof = new Profiler ();
+        for (int i=0; i<COUNT; i++) {
+            assertTrue (s.equals (sp.in (k)));
+            if (i % 100 == 0)
+                prof.checkPoint ("in " + i);
+        }
+        // prof.dump (System.err, "SingleKeyLoad in  >");
+        assertTrue (sp.rdp (k) == null);
+    }
+    public void testTemplate () throws Exception {
+        String key = "TemplateTest_Key";
+        sp.out (key, "Value 1");
+        sp.out (key, "Value 2");
+        sp.out (key, "Value 3");
+
+        String k2r = (String)sp.rdp (new MD5Template (key, "Value 2"));
+        assertEquals (k2r, "Value 2");
+
+        String k2i = (String)sp.inp (new MD5Template (key, "Value 2"));
+        assertEquals (k2i, "Value 2");
+        assertEquals ("Value 1", (String) sp.inp (key));
+        assertEquals ("Value 3", (String) sp.inp (key));
+    }
+}
+
---
+++ modules/jpos/test/org/jpos/space/TSpaceTestCase.java
@@ -0,0 +1,156 @@
+package org.jpos.space;
+
+import org.jpos.util.Profiler;
+import junit.framework.TestCase;
+
+public class TSpaceTestCase extends TestCase implements SpaceListener {
+    TSpace sp;
+    public static final int COUNT = 100000;
+    Object notifiedValue = null;
+
+    public void setUp () {
+        sp = new TSpace();
+    }
+    public void testSimpleOut() {
+        sp.out ("testSimpleOut_Key", "ABC");
+        sp.out ("testSimpleOut_Key", "XYZ");
+
+        assertEquals ("ABC", sp.rdp ("testSimpleOut_Key"));
+        assertEquals ("ABC", sp.inp ("testSimpleOut_Key"));
+        assertEquals ("XYZ", sp.rdp ("testSimpleOut_Key"));
+        assertEquals ("XYZ", sp.inp ("testSimpleOut_Key"));
+        assertNull (sp.rdp ("Test"));
+        assertNull (sp.inp ("Test"));
+    }
+    public void testExpiration () {
+        sp.out ("testExpiration_Key", "ABC", 50);
+        assertEquals ("ABC", sp.rdp ("testExpiration_Key"));
+        try {
+            Thread.sleep (60);
+        } catch (InterruptedException e) { }
+        assertNull ("ABC", sp.rdp ("testExpiration_Key"));
+    }
+    public void testOutRdpInpRdp() throws Exception {
+        Object o = new Boolean (true);
+        String k = "testOutRdpInpRdp_Key";
+        sp.out (k, o);
+        assertTrue (o.equals (sp.rdp (k)));
+        assertTrue (o.equals (sp.rd  (k)));
+        assertTrue (o.equals (sp.rd  (k, 1000)));
+        assertTrue (o.equals (sp.inp (k)));
+        assertNull (sp.rdp (k));
+        assertNull (sp.rd  (k, 100));
+    }
+    public void testMultiKeyLoad() throws Exception {
+        String s = "The quick brown fox jumped over the lazy dog";
+        Profiler prof = new Profiler ();
+        for (int i=0; i<COUNT; i++) {
+            sp.out ("testMultiKeyLoad_Key" + Integer.toString (i), s, 60000);
+        }
+        // prof.dump (System.err, "MultiKeyLoad out >");
+        prof = new Profiler ();
+        for (int i=0; i<COUNT; i++) {
+            assertTrue (s.equals (sp.in ("testMultiKeyLoad_Key" + Integer.toString (i))));
+        }
+        // prof.dump (System.err, "MultiKeyLoad in  >");
+    }
+    public void testSingleKeyLoad() throws Exception {
+        String s = "The quick brown fox jumped over the lazy dog";
+        String k = "testSingleKeyLoad_SingleKey";
+        Profiler prof = new Profiler ();
+        for (int i=0; i<COUNT; i++) {
+            sp.out (k, s, 60000);
+        }
+        // prof.dump (System.err, "SingleKeyLoad out >");
+        prof = new Profiler ();
+        for (int i=0; i<COUNT; i++) {
+            assertTrue (s.equals (sp.in (k)));
+        }
+        // prof.dump (System.err, "SingleKeyLoad in  >");
+
+        assertNull (sp.rdp (k));
+    }
+    public void testGC () throws Exception {
+        sp.out ("testGC_Key", "ABC", 50);
+        sp.out ("testGC_Key", "XYZ", 50);
+        assertEquals ("ABC", sp.rdp ("testGC_Key"));
+        try {
+            Thread.sleep (60);
+        } catch (InterruptedException e) { }
+
+        assertEquals ("testGC_Key", sp.getKeysAsString());
+        sp.gc();
+        assertEquals ("", sp.getKeysAsString());
+        sp.gc();
+    }
+    public void testTemplate () throws Exception {
+        final String KEY = "TestTemplate_Key";
+        sp.out (KEY, "123");
+        sp.out (KEY, "456"); 
+        sp.out (KEY, "789"); 
+
+        Template tmpl = new ObjectTemplate (KEY, "456");
+        assertEquals (sp.rdp (KEY),  "123");
+        assertEquals (sp.rdp (tmpl), "456");
+        assertEquals (sp.rdp (KEY),  "123");
+        assertEquals (sp.inp (tmpl), "456");
+        assertNull (sp.rdp (tmpl));
+        assertNull (sp.inp (tmpl));
+        assertEquals (sp.rdp (KEY),  "123");
+        assertEquals (sp.inp (KEY),  "123");
+        assertEquals (sp.rdp (KEY),  "789");
+        assertEquals (sp.inp (KEY),  "789");
+        assertNull (sp.rdp (KEY));
+        assertNull (sp.inp (KEY));
+    }
+    public void testMD5Template () throws Exception {
+        final String KEY = "TestMD5Template_Key";
+        sp.out (KEY, "123");
+        sp.out (KEY, "456"); 
+        sp.out (KEY, "789"); 
+
+        Template tmpl = new MD5Template (KEY, "456");
+        assertEquals (sp.rdp (KEY),  "123");
+        assertEquals (sp.rdp (tmpl), "456");
+        assertEquals (sp.rdp (KEY),  "123");
+        assertEquals (sp.inp (tmpl), "456");
+        assertNull (sp.rdp (tmpl));
+        assertNull (sp.inp (tmpl));
+        assertEquals (sp.rdp (KEY),  "123");
+        assertEquals (sp.inp (KEY),  "123");
+        assertEquals (sp.rdp (KEY),  "789");
+        assertEquals (sp.inp (KEY),  "789");
+        assertNull (sp.rdp (KEY));
+        assertNull (sp.inp (KEY));
+    }
+    public void testNotify() {
+        sp.addListener ("TestDelayNotify_Key", this, 500);
+        sp.out ("TestNotify_Key", "ABCCBA");
+        assertNull (notifiedValue);
+
+        sp.addListener ("TestNotify_Key", this);
+        sp.out ("TestNotify_Key", "ABCCBA");
+        assertEquals (notifiedValue, "ABCCBA");
+
+        sp.out ("TestNotify_Key", "012345");
+        assertEquals (notifiedValue, "012345");
+
+        assertEquals (sp.inp ("TestNotify_Key"), "ABCCBA");
+        assertEquals (sp.inp ("TestNotify_Key"), "ABCCBA");
+        assertEquals (sp.inp ("TestNotify_Key"), "012345");
+
+        sp.out ("TestDelayNotify_Key", "OLD");
+        assertEquals (notifiedValue, "OLD");
+        try {
+            Thread.sleep (600);
+        } catch (InterruptedException e) { }
+        sp.out ("TestDelayNotify_Key", "NEW");
+        assertEquals (notifiedValue, "OLD");  // still OLD
+        assertEquals (sp.inp ("TestDelayNotify_Key"), "OLD");
+        assertEquals (sp.inp ("TestDelayNotify_Key"), "NEW");
+    }
+    public void notify (Object key, Object value) {
+        this.notifiedValue = value;
+    }
+}
+
