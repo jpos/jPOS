2a2e3ca28a9bdacc34208379645df90494d06b34
Alejandro Revilla <apr@jpos.org>
2006-05-19 14:38:24 +0000

    simple testbed to play with the TransactionManager

---
+++ modules/txnmgr/test/org/jpos/transaction/TransactionManagerTestCase.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2006 jPOS.org 
+ *
+ * See terms of license at http://jpos.org/license.html
+ *
+ */
+
+package org.jpos.transaction;
+
+import junit.framework.*;
+import org.jpos.q2.Q2;
+import org.jpos.iso.ISOUtil;
+import org.jpos.space.Space;
+import org.jpos.space.SpaceFactory;
+
+public class TransactionManagerTestCase extends TestCase {
+    Q2 q2;
+    Space sp;
+    public static String QUEUE = "TXNMGRTEST";
+
+    public void setUp () throws Exception {
+        sp = SpaceFactory.getSpace();
+        q2 = new Q2(new String[] { "-d", "../test/org/jpos/transaction" });
+        new Thread() {
+            public void run() {
+                try {
+                    q2.start();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+        Thread.sleep (5000); // let the thing start
+    }
+    public void testSimpleTransaction() {
+        Context ctx = new Context();
+        ctx.put ("volatile", "the quick brown fox");
+        ctx.put ("persistent", "jumped over the lazy dog", true);
+        sp.out (QUEUE, ctx);
+    }
+    public void tearDown() throws Exception {
+        q2.shutdown();
+    }
+}
+
