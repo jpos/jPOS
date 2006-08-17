4773bc4ebbe64561593098c814b28e518018aca9
Alejandro Revilla <apr@jpos.org>
2006-08-17 15:15:35 +0000

    simple "trace" participant used for development/debugging purposes

---
+++ jpos6/modules/txnmgr/src/org/jpos/transaction/participant/Trace.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2006 jPOS.org 
+ *
+ * See terms of license at http://jpos.org/license.html
+ *
+ */
+
+package org.jpos.transaction.participant;
+
+import java.io.Serializable;
+import org.jpos.transaction.Context;
+import org.jpos.transaction.AbortParticipant;
+import org.jpos.core.Configurable;
+import org.jpos.core.Configuration;
+
+public class Trace implements AbortParticipant, Configurable {
+    String trace;
+    public int prepare (long id, Serializable o) {
+        Context ctx = (Context) o;
+        ctx.checkPoint ("prepare:" + trace);
+        return PREPARED | READONLY;
+    }
+    public void commit (long id, Serializable o) { 
+        Context ctx = (Context) o;
+        ctx.checkPoint ("commit:" + trace);
+    }
+    public void abort  (long id, Serializable o) { 
+        Context ctx = (Context) o;
+        ctx.checkPoint ("abort:" + trace);
+    }
+    public int prepareForAbort (long id, Serializable o) { 
+        Context ctx = (Context) o;
+        ctx.checkPoint ("prepareForAbort:" + trace);
+        return PREPARED | READONLY;
+    }
+    public void setConfiguration (Configuration cfg) {
+        this.trace = cfg.get ("trace", this.getClass().getName().toString());
+    }
+}
+
