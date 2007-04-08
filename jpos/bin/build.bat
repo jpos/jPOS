3d17ef1ef07a5fdb4585b575b02710470cc5f34a
Alejandro Revilla <apr@jpos.org>
2007-04-08 22:17:40 +0000

    Move 1.5 HEAD into branches/v1_5_3

--- jpos/bin/build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM $Id$
-set JAVA=%JAVA_HOME%\bin\java
-set cp=%CLASSPATH%
-for %%i in (ext\*.jar) do call bin\cp.bat %%i
-for %%i in (lib\*.jar) do call bin\cp.bat %%i
-set CP=%JAVA_HOME%\lib\tools.jar;%CP%
-%JAVA% -classpath "%CP%" -Dant.home=lib org.apache.tools.ant.Main %1 %2 %3 %4 %5 %6 -buildfile src/build.xml
-set CP=
