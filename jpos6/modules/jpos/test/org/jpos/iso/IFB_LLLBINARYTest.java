2ab01fcf24e236bc6b8f3043753a34960e87ba25
Mark Salter <marksalter@talktalk.net>
2007-07-04 19:39:17 +0000

    Include test for field content over 16 bytes to demonstrate the non hex flavour of the length structure.

--- jpos6/modules/jpos/test/org/jpos/iso/IFB_LLLBINARYTest.java
+++ jpos6/modules/jpos/test/org/jpos/iso/IFB_LLLBINARYTest.java
@@ -40,4 +40,31 @@ public class IFB_LLLBINARYTest extends TestCase
         packager.unpack(unpack, packager.pack(f), 0);
         TestUtils.assertEquals(origin, (byte[])unpack.getValue());
     }
+    
+    public void testPackGreaterThan16() throws Exception
+    {
+        ISOBinaryField field = new ISOBinaryField(1, new byte[] {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 ,0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11});
+        IFB_LLLBINARY packager = new IFB_LLLBINARY(1, "Should be 1234");
+        TestUtils.assertEquals(new byte[] {0x00, 0x17, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 ,0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11}, packager.pack(field));
+    }
+
+    public void testUnpackGreaterThan16() throws Exception
+    {
+        byte[] raw = new byte[] {0x00, 0x17, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 ,0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11};
+        IFB_LLLBINARY packager = new IFB_LLLBINARY(1, "Should be 17 bytes 01 through 11");
+        ISOBinaryField field = new ISOBinaryField(1);
+        packager.unpack(field, raw, 0);
+        TestUtils.assertEquals(new byte[] {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 ,0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11}, (byte[])field.getValue());
+    }
+
+    public void testReversabilityGreaterThan16() throws Exception
+    {
+        byte[] origin = new byte[] {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 ,0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11};
+        ISOBinaryField f = new ISOBinaryField(1, origin);
+        IFB_LLLBINARY packager = new IFB_LLLBINARY(1, "Should be x'0102030405060708090a0b0c0d0e0f1011'");
+
+        ISOBinaryField unpack = new ISOBinaryField(12);
+        packager.unpack(unpack, packager.pack(f), 0);
+        TestUtils.assertEquals(origin, (byte[])unpack.getValue());
+    }
 }
