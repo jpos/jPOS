a80b50808c57736a0b338176700c2dd0b48a5c25
Mark Salter <marksalter@talktalk.net>
2007-07-14 18:56:08 +0000

    Watch only for message length, which includes itself in the length.

---
+++ modules/jpos/src/org/jpos/iso/channel/AmexChannel.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2004 jPOS.org 
+ *
+ * See terms of license at http://jpos.org/license.html
+ *
+ */
+
+package org.jpos.iso.channel;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+import org.jpos.iso.BaseChannel;
+import org.jpos.iso.ISOChannel;
+import org.jpos.iso.ISOException;
+import org.jpos.iso.ISOMsg;
+import org.jpos.iso.ISOPackager;
+import org.jpos.util.LogEvent;
+import org.jpos.util.Logger;
+
+/**
+ * ISOChannel implementation - American Express
+ * 
+ * @author marksalter@dsl.pipex.com
+ * @version $Id: AmexChannel.java,v 1.5 2006/01/27 10:36:18 mark Exp $
+ * @see ISOMsg
+ * @see ISOException
+ * @see ISOChannel
+ */
+public class AmexChannel extends BaseChannel {
+
+	/**
+	 * Public constructor (used by Class.forName("...").newInstance())
+	 */
+	public AmexChannel() {
+		super();
+	}
+
+	/**
+	 * Construct client ISOChannel
+	 * 
+	 * @param host
+	 *            server TCP Address
+	 * @param port
+	 *            server port number
+	 * @param p
+	 *            an ISOPackager (should be ISO87BPackager)
+	 * @see org.jpos.iso.packager.ISO87BPackager
+	 */
+	public AmexChannel(String host, int port, ISOPackager p) {
+		super(host, port, p);
+	}
+
+	/**
+	 * Construct server ISOChannel
+	 * 
+	 * @param p
+	 *            an ISOPackager (should be ISO87BPackager)
+	 * @exception IOException
+	 * @see org.jpos.iso.packager.ISO87BPackager
+	 */
+	public AmexChannel(ISOPackager p) throws IOException {
+		super(p);
+	}
+
+	/**
+	 * constructs a server ISOChannel associated with a Server Socket
+	 * 
+	 * @param p
+	 *            an ISOPackager
+	 * @param serverSocket
+	 *            where to accept a connection
+	 * @exception IOException
+	 * @see ISOPackager
+	 */
+	public AmexChannel(ISOPackager p, ServerSocket serverSocket)
+			throws IOException {
+		super(p, serverSocket);
+	}
+
+	protected void sendMessageLength(int len) throws IOException {
+		serverOut.write(len+2 >> 8);
+		serverOut.write(len+2);
+	}
+
+	protected int getMessageLength() throws IOException, ISOException {
+		int l = 0;
+		byte[] b = new byte[2];
+		// ignore polls (0 message length)
+		while (l == 0) {
+			serverIn.readFully(b, 0, 2);
+			l = ((((int) b[0]) & 0xFF) << 8) | (((int) b[1]) & 0xFF);
+			if (l == 0) {
+				serverOut.write(b);
+				serverOut.flush();
+				Logger.log(new LogEvent(this, "poll"));
+			}
+		}
+		// Message length includes length itself, so adjust the message total down by 2
+		l = l - 2;
+
+		return l;
+	}
+
+}
