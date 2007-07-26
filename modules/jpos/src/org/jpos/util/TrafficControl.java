356a5a412c79cf3158c158209ef311628b23b6ae
Alejandro Revilla <apr@jpos.org>
2007-07-26 01:51:15 +0000

    initial take at a TrafficControl component

---
+++ modules/jpos/src/org/jpos/util/TrafficControl.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2007 jPOS.org 
+ *
+ * See terms of license at http://jpos.org/license.html
+ *
+ */
+
+package org.jpos.util;
+
+/**
+ * TrafficControl can be used to limit the throughput 
+ * of a system to a maximum number of transactions in 
+ * a given period of time.
+ */
+public class TrafficControl {
+    private int[] period;
+    private int[] max;
+    private int[] cnt;
+    private long[] start;
+    private long[] sleep;
+
+    /**
+     * @param maxTransactions ditto
+     * @param periodInMillis ditto
+     */
+    public TrafficControl (int maxTransactions, int periodInMillis) {
+        this (new int[] { maxTransactions },
+              new int[] { periodInMillis });
+    }
+    /**
+     * @param maxTransactions ditto
+     * @param periodInMillis ditto
+     */
+    public TrafficControl (int[] maxTransactions, int[] periodInMillis) {
+        super();
+        int l = maxTransactions.length;
+        period = new int[l];
+        max = new int[l];
+        cnt = new int[l];
+        start = new long[l];
+        sleep = new long[l];
+        for (int i=0; i<l; i++) {
+            this.max[i]    = maxTransactions[i];
+            this.period[i] = periodInMillis[i];
+            this.sleep[i]  = Math.min(Math.max (periodInMillis[i]/10, 500L),50L);;
+            this.start[i]  = System.currentTimeMillis();
+        }
+    }
+
+    /**
+     * control should be called on every transaction.
+     * it may sleep for a while in order to control the system throughput
+     */
+    public void control() {
+        for (int i=0; i<cnt.length; i++) {
+            synchronized (this) {
+                cnt[i]++;
+            }
+            do {
+                if (cnt[i] > max[i]) {
+                    try { 
+                        Thread.sleep (sleep[i]); 
+                    } catch (InterruptedException e) { }
+                }
+                synchronized (this) {
+                    long now = System.currentTimeMillis();
+                    if (now - start[i] > period[i]) {
+                        long elapsed = now - start[i];
+                        int  allowed = (int) ((elapsed * max[i]) / period[i]);
+                        start[i] = now;
+                        cnt[i] = Math.max (cnt[i] - allowed, 0);
+                    }
+                }
+            } while (cnt[i] > max[i]);
+        }
+    }
+}
+
---
+++ modules/jpos/test/org/jpos/util/TrafficControlTestCase.java
@@ -0,0 +1,92 @@
+package org.jpos.util;
+
+import junit.framework.*;
+
+
+public class TrafficControlTestCase extends TestCase {
+    public void testSingleThread () throws Exception {
+        TrafficControl tc = new TrafficControl (2, 1000);
+        long start = System.currentTimeMillis();
+        tc.control(); 
+        assertTrue (
+            "Elapsed time should be less than one second", 
+            System.currentTimeMillis() - start < 1000L
+        );
+        tc.control();
+        assertTrue (
+            "Elapsed time should still be less than one second", 
+            System.currentTimeMillis() - start < 1000L
+        );
+        tc.control();
+        assertTrue (
+            "Elapsed time should be greater than one second", 
+            System.currentTimeMillis() - start > 1000L
+        );
+        tc.control();
+        assertTrue (
+            "second transaction should be less than two seconds", 
+            System.currentTimeMillis() - start < 2000L
+        );
+    }
+    public void testFifty () throws Exception {
+        TrafficControl tc = new TrafficControl (10, 1000);
+        long start = System.currentTimeMillis();
+        for (int i=0; i<50; i++)
+            tc.control();
+
+        long elapsed =  System.currentTimeMillis() - start;
+        assertTrue (
+            "50 transactions should take at least 4 seconds but took " + elapsed, 
+            elapsed >= 4000L
+        );
+        assertTrue (
+            "50 transactions shouldn't take more than aprox 4 seconds but took " + elapsed, 
+            elapsed < 4300L
+        );
+    }
+    public void testDualPeriod () throws Exception {
+        TrafficControl tc = new TrafficControl (
+            new int[] { 100, 150 }, 
+            new int[] { 1000, 5000 }
+        );
+        long start = System.currentTimeMillis();
+        for (int i=0; i<100; i++)
+            tc.control();
+
+        long elapsed =  System.currentTimeMillis() - start;
+        assertTrue (
+            "100 initial transactions should take more than about one second but took " + elapsed, 
+            elapsed <= 1000L
+        );
+        for (int i=0; i<100; i++)
+            tc.control();
+
+        elapsed =  System.currentTimeMillis() - start;
+        assertTrue (
+            "100 additional transactions should take more than five seconds but took " + elapsed, 
+            elapsed > 5000L
+        );
+    }
+    public void testMultiThread() throws Exception {
+        final TrafficControl tc = new TrafficControl (2, 1000);
+        long start = System.currentTimeMillis();
+        Thread[] t = new Thread[10];
+        for (int i=0; i<10; i++) {
+            t[i] = new Thread() {
+                public void run() {
+                    tc.control();
+                }
+            };
+            t[i].start();
+        }
+        for (int i=0; i<10; i++) {
+            t[i].join();
+        }
+        long elapsed =  System.currentTimeMillis() - start;
+        assertTrue (
+            "10 transactions should take about four seconds but took " + elapsed, 
+            elapsed > 4000L && elapsed < 5000L
+        );
+    }
+}
+
