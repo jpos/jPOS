28df5d1972aacb7cae50e7a4eccf4cdc57ea370b
Alejandro Revilla <apr@jpos.org>
2007-11-22 15:00:47 +0000

    Upgrade to AGPL v3.0

--- modules/jpos/src/org/jpos/iso/gui/ISOMsgPanel.java
+++ modules/jpos/src/org/jpos/iso/gui/ISOMsgPanel.java
@@ -1,91 +1,19 @@
 /*
- * Copyright (c) 2000 jPOS.org.  All rights reserved.
+ * jPOS Project [http://jpos.org]
+ * Copyright (C) 2000-2007 Alejandro P. Revilla
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
  *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:
- *    "This product includes software developed by the jPOS project 
- *    (http://www.jpos.org/)". Alternately, this acknowledgment may 
- *    appear in the software itself, if and wherever such third-party 
- *    acknowledgments normally appear.
- *
- * 4. The names "jPOS" and "jPOS.org" must not be used to endorse 
- *    or promote products derived from this software without prior 
- *    written permission. For written permission, please contact 
- *    license@jpos.org.
- *
- * 5. Products derived from this software may not be called "jPOS",
- *    nor may "jPOS" appear in their name, without prior written
- *    permission of the jPOS project.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  
- * IN NO EVENT SHALL THE JPOS PROJECT OR ITS CONTRIBUTORS BE LIABLE FOR 
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS 
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING 
- * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
- * POSSIBILITY OF SUCH DAMAGE.
- * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the jPOS Project.  For more
- * information please see <http://www.jpos.org/>.
- */
-
-/**
- * ISOMsgPanel
- * Swing based GUI to ISOMsg
- * @author apr@cs.com.uy
- * @see org.jpos.iso.ISOMsg
- */
-
-/*
- * $Log$
- * Revision 1.10  2006/03/29 20:32:22  marklsalter
- * Ask the message to be displayed for the highest field, rather than assume 128.
- *
- * Revision 1.9  2003/05/16 04:15:19  alwyns
- * Import cleanups.
- *
- * Revision 1.8  2002/12/16 12:47:25  apr
- * Minor changes to reduce new Doclet warnings.
- *
- * Revision 1.7  2000/11/02 12:09:18  apr
- * Added license to every source file
- *
- * Revision 1.6  2000/03/01 14:44:45  apr
- * Changed package name to org.jpos
- *
- * Revision 1.5  1999/09/06 17:20:21  apr
- * Added Logger SubSystem
- *
- * Revision 1.4  1999/08/06 13:52:08  apr
- * Added getValueAdjusting() check to avoid inner ISOMsgs showing twice
- *
- * Revision 1.3  1999/08/06 11:40:12  apr
- * expand -4
- *
- * Revision 1.2  1999/05/18 14:50:10  apr
- * Show ISOMsg fields in new frame
- *
- * Revision 1.1  1999/05/18 12:02:59  apr
- * Added GUI package
- *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package org.jpos.iso.gui;
