aed6ce532b5a1ae0c445be0bb56ed7cd031cd954
Alejandro Revilla <apr@jpos.org>
2007-11-22 15:00:47 +0000

    Upgrade to AGPL v3.0

--- jpos6/modules/jpos/src/org/jpos/iso/gui/ISORequestListenerPanel.java
+++ jpos6/modules/jpos/src/org/jpos/iso/gui/ISORequestListenerPanel.java
@@ -1,90 +1,19 @@
 /*
- * Copyright (c) 2000 jPOS.org.  All rights reserved.
+ * jPOS Project [http://jpos.org]
+ * Copyright (C) 2000-2007 Alejandro P. Revilla
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
  *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:
- *    "This product includes software developed by the jPOS project 
- *    (http://www.jpos.org/)". Alternately, this acknowledgment may 
- *    appear in the software itself, if and wherever such third-party 
- *    acknowledgments normally appear.
- *
- * 4. The names "jPOS" and "jPOS.org" must not be used to endorse 
- *    or promote products derived from this software without prior 
- *    written permission. For written permission, please contact 
- *    license@jpos.org.
- *
- * 5. Products derived from this software may not be called "jPOS",
- *    nor may "jPOS" appear in their name, without prior written
- *    permission of the jPOS project.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  
- * IN NO EVENT SHALL THE JPOS PROJECT OR ITS CONTRIBUTORS BE LIABLE FOR 
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS 
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING 
- * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
- * POSSIBILITY OF SUCH DAMAGE.
- * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the jPOS Project.  For more
- * information please see <http://www.jpos.org/>.
- */
-
-/**
- * ISORequestListenerPanel
- * Allows visualization of unhandled ISOMUX messages
- * @author apr@cs.com.uy
- * @see org.jpos.iso.ISOMsg
- * @see org.jpos.iso.ISOMUX
- * @see ISOMsgPanel
- */
-
-/*
- * $Log$
- * Revision 1.9  2003/10/13 10:30:22  apr
- * tabs expanded to 8 spaces
- *
- * Revision 1.8  2003/05/16 04:15:18  alwyns
- * Import cleanups.
- *
- * Revision 1.7  2000/11/02 12:09:18  apr
- * Added license to every source file
- *
- * Revision 1.6  2000/04/14 18:27:42  apr
- * ISORequestListener may not be Observable
- *
- * Revision 1.5  2000/03/01 14:44:45  apr
- * Changed package name to org.jpos
- *
- * Revision 1.4  1999/09/06 17:20:22  apr
- * Added Logger SubSystem
- *
- * Revision 1.3  1999/08/06 11:40:12  apr
- * expand -4
- *
- * Revision 1.2  1999/07/29 15:55:12  apr
- * Added LOG_CAPACITY checks
- *
- * Revision 1.1  1999/05/18 12:02:59  apr
- * Added GUI package
- *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package org.jpos.iso.gui;
--- jpos6/modules/q2/src/org/jpos/q2/qbean/SystemMonitorMBeanDescription.java
+++ jpos6/modules/q2/src/org/jpos/q2/qbean/SystemMonitorMBeanDescription.java
@@ -1,3 +1,21 @@
+/*
+ * jPOS Project [http://jpos.org]
+ * Copyright (C) 2000-2007 Alejandro P. Revilla
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /* * Generated file - Do not edit! */package org.jpos.q2.qbean;import mx4j.MBeanDescriptionAdapter;import java.lang.reflect.Method;import java.lang.reflect.Constructor;/** * MBean description. * @author apr@cs.com.uy
  * @version $Id: SystemMonitor.java,v 1.2 2005/11/07 20:12:11 marklsalter Exp $
  * @see Logger
