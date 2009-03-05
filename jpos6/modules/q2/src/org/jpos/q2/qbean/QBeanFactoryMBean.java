ec377a466b88a890b49d8af847bf1c560cc4807e
Alejandro Revilla <apr@jpos.org>
2009-03-05 11:24:43 +0000

    update copyright year

--- jpos6/modules/q2/src/org/jpos/q2/qbean/QBeanFactoryMBean.java
+++ jpos6/modules/q2/src/org/jpos/q2/qbean/QBeanFactoryMBean.java
@@ -1,6 +1,6 @@
 /*
  * jPOS Project [http://jpos.org]
- * Copyright (C) 2000-2008 Alejandro P. Revilla
+ * Copyright (C) 2000-2009 Alejandro P. Revilla
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package org.jpos.q2.qbean;
 
 import org.jpos.q2.QBeanSupportMBean;
