3c205f36878d6fe08f9542fc8dc05e86ce6adc88
Alejandro Revilla <apr@jpos.org>
2009-07-18 23:30:10 +0000

    Minor cleanup, removed some warnings

--- modules/q2mod_jpos/src/org/jpos/q2/iso/OneShotChannelAdaptor.java
+++ modules/q2mod_jpos/src/org/jpos/q2/iso/OneShotChannelAdaptor.java
@@ -18,22 +18,11 @@
 
 package org.jpos.q2.iso;
 
-import java.io.IOException;
-import java.util.Date;
-import java.util.Iterator;
-import org.jpos.iso.ISOMsg;
-import org.jpos.iso.ISOUtil;
 import org.jpos.space.Space;
 import org.jpos.space.SpaceFactory;
 import org.jpos.core.Configuration;
 import org.jpos.core.ConfigurationException;
-import org.jpos.iso.FactoryChannel;
-import org.jpos.iso.Channel;
-import org.jpos.iso.ISOChannel;
-import org.jpos.iso.ISOClientSocketFactory;
-import org.jpos.iso.ISOPackager;
-import org.jpos.iso.ISOFilter;
-import org.jpos.iso.FilteredChannel;
+import org.jpos.iso.*;
 import org.jpos.q2.QBeanSupport;
 import org.jpos.q2.QFactory;
 import org.jpos.util.LogSource;
@@ -60,14 +49,13 @@ import org.jdom.Element;
  * @author Alejandro Revilla
  * @author Thomas L. Kjeldsen
  * @version $Revision$ $Date$
- * @jmx:mbean description="ISOChannel wrapper" 
- *                extends="org.jpos.q2.QBeanSupportMBean"
+ *
  */
 public class OneShotChannelAdaptor 
     extends QBeanSupport
     implements OneShotChannelAdaptorMBean, Channel
 {
-    Space sp;
+    Space<String,Object> sp;
     Configuration cfg;
     String in, out;
     long delay;
@@ -77,13 +65,14 @@ public class OneShotChannelAdaptor
         super ();
     }
 
-    private Space grabSpace (Element e) {
-        return SpaceFactory.getSpace (e != null ? e.getText() : "");
+    @SuppressWarnings("unchecked")
+    private Space<String,Object> grabSpace (Element e) {
+        return (Space<String,Object>) SpaceFactory.getSpace (e != null ? e.getText() : "");
     }
 
     public void initAdaptor() {
         Element persist = getPersist ();
-        sp = grabSpace (persist.getChild ("space")); 
+        sp = grabSpace (persist.getChild ("space"));
         in = persist.getChildTextTrim ("in");
         out = persist.getChildTextTrim ("out");
         delay = 5000;
@@ -131,7 +120,7 @@ public class OneShotChannelAdaptor
     /**
      * Queue a message to be transmitted by this adaptor
      * @param m message to send
-     * @param timeout 
+     * @param timeout in millis
      */
     public void send (ISOMsg m, long timeout) {
         sp.out (in, m, timeout);
@@ -221,7 +210,7 @@ public class OneShotChannelAdaptor
             String packagerName = e.getAttributeValue ("packager");
 
             ISOChannel channel   = (ISOChannel) f.newInstance (channelName);
-            ISOPackager packager = null;
+            ISOPackager packager;
             if (packagerName != null) {
                 packager = (ISOPackager) f.newInstance (packagerName);
                 channel.setPackager (packager);
@@ -242,32 +231,28 @@ public class OneShotChannelAdaptor
         private void addFilters (FilteredChannel channel, Element e, QFactory fact) 
             throws ConfigurationException
         {
-            Iterator iter = e.getChildren ("filter").iterator();
-            while (iter.hasNext()) {
-                Element f = (Element) iter.next();
-                String clazz = f.getAttributeValue ("class");
-                ISOFilter filter = (ISOFilter) fact.newInstance (clazz);
-                fact.setLogger        (filter, f);
-                fact.setConfiguration (filter, f);
-                String direction = f.getAttributeValue ("direction");
+            for (Object o : e.getChildren("filter")) {
+                Element f = (Element) o;
+                String clazz = f.getAttributeValue("class");
+                ISOFilter filter = (ISOFilter) fact.newInstance(clazz);
+                fact.setLogger(filter, f);
+                fact.setConfiguration(filter, f);
+                String direction = f.getAttributeValue("direction");
                 if (direction == null)
-                    channel.addFilter (filter);
-                else if ("incoming".equalsIgnoreCase (direction))
-                    channel.addIncomingFilter (filter);
-                else if ("outgoing".equalsIgnoreCase (direction))
-                    channel.addOutgoingFilter (filter);
-                else if ("both".equalsIgnoreCase (direction)) {
-                    channel.addIncomingFilter (filter);
-                    channel.addOutgoingFilter (filter);
+                    channel.addFilter(filter);
+                else if ("incoming".equalsIgnoreCase(direction))
+                    channel.addIncomingFilter(filter);
+                else if ("outgoing".equalsIgnoreCase(direction))
+                    channel.addOutgoingFilter(filter);
+                else if ("both".equalsIgnoreCase(direction)) {
+                    channel.addIncomingFilter(filter);
+                    channel.addOutgoingFilter(filter);
                 }
             }
         }
     
     }
 
-    /**
-     * @jmx:managed-attribute description="input queue"
-     */
     public synchronized void setInQueue (String in) {
         String old = this.in;
         this.in = in;
@@ -277,70 +262,51 @@ public class OneShotChannelAdaptor
         getPersist().getChild("in").setText (in);
         setModified (true);
     }
-    /**
-     * @jmx:managed-attribute description="input queue"
-     */
+
     public String getInQueue () {
         return in;
     }
-    /**
-     * @jmx:managed-attribute description="output queue"
-     */
+
     public synchronized void setOutQueue (String out) {
         this.out = out;
         getPersist().getChild("out").setText (out);
         setModified (true);
     }
-    /**
-     * @jmx:managed-attribute description="output queue"
-     */
+
     public String getOutQueue () {
         return out;
     }
-    /**
-     * @jmx:managed-attribute description="remote host address"
-     */
+
     public synchronized void setHost (String host) {
         setProperty (getProperties ("channel"), "host", host);
         setModified (true);
     }
-    /**
-     * @jmx:managed-attribute description="remote host address"
-     */
+
     public String getHost () {
         return getProperty (getProperties ("channel"), "host");
     }
-    /**
-     * @jmx:managed-attribute description="remote port"
-     */
+
     public synchronized void setPort (int port) {
         setProperty (
             getProperties ("channel"), "port", Integer.toString (port)
         );
         setModified (true);
     }
-    /**
-     * @jmx:managed-attribute description="remote port"
-     */
     public int getPort () {
         int port = 0;
         try {
             port = Integer.parseInt (
                 getProperty (getProperties ("channel"), "port")
             );
-        } catch (NumberFormatException e) { }
+        } catch (NumberFormatException e) {
+            getLog().error (e);
+        }
         return port;
     }
-    /**
-     * @jmx:managed-attribute description="socket factory" 
-     */
     public synchronized void setSocketFactory (String sFac) {
         setProperty(getProperties("channel"), "socketFactory", sFac);
         setModified(true);
     }
-    /**
-     * @jmx:managed-attribute description="socket factory" 
-     */
     public String getSocketFactory() {
         return getProperty(getProperties ("channel"), "socketFactory");
     }
