96ad8a449eca3eba14c1858b81604de17d123a51
Mark Salter <marksalter@talktalk.net>
2009-07-23 20:03:39 +0000

    Correctly name 'Hex'Channel

---
+++ modules/jpos/src/org/jpos/iso/channel/BCDChannel.java
@@ -0,0 +1,131 @@
+/*
+ * jPOS Project [http://jpos.org]
+ * Copyright (C) 2000-2009 Alejandro P. Revilla
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jpos.iso.channel;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+import org.jpos.iso.BaseChannel;
+import org.jpos.iso.ISOChannel;
+import org.jpos.iso.ISOException;
+import org.jpos.iso.ISOMsg;
+import org.jpos.iso.ISOPackager;
+import org.jpos.iso.ISOUtil;
+import org.jpos.util.LogEvent;
+import org.jpos.util.Logger;
+
+/**
+ * Talks with TCP based NCCs
+ * Sends [LEN][TPDU][ISOMSG]
+ * (len=2 bytes HEX)
+ *
+ * @author Mladen Mrkic <mmrkic@arius.co.yu>
+ * @version $Revision: 2706 $ $Date: 2009-03-05 11:24:43 +0000 (Thu, 05 Mar 2009) $
+ * @see ISOMsg
+ * @see ISOException
+ * @see ISOChannel
+ */
+public class BCDChannel extends BaseChannel {
+    /**
+     * Public constructor 
+     */
+    public BCDChannel () {
+        super();
+    }
+    /**
+     * Construct client ISOChannel
+     * @param host  server TCP Address
+     * @param port  server port number
+     * @param p     an ISOPackager
+     * @param TPDU  an optional raw header (i.e. TPDU)
+     * @see ISOPackager
+     */
+    public BCDChannel (String host, int port, ISOPackager p, byte[] TPDU) {
+        super(host, port, p);
+        this.header = TPDU;
+    }
+    /**
+     * Construct server ISOChannel
+     * @param p     an ISOPackager
+     * @param TPDU  an optional raw header (i.e. TPDU)
+     * @exception IOException
+     * @see ISOPackager
+     */
+    public BCDChannel (ISOPackager p, byte[] TPDU) throws IOException {
+        super(p);
+        this.header = TPDU;
+    }
+    /**
+     * constructs server ISOChannel associated with a Server Socket
+     * @param p     an ISOPackager
+     * @param TPDU  an optional raw header (i.e. TPDU)
+     * @param serverSocket where to accept a connection
+     * @exception IOException
+     * @see ISOPackager
+     */
+    public BCDChannel (ISOPackager p, byte[] TPDU, ServerSocket serverSocket) 
+        throws IOException
+    {
+        super(p, serverSocket);
+        this.header = TPDU;
+    }
+    protected void sendMessageLength(int len) throws IOException {
+        try {
+            serverOut.write (                                                                                                         
+                ISOUtil.str2bcd (                                                                                                     
+                    ISOUtil.zeropad (Integer.toString (len), 4), true
+                )
+            );
+        } 
+        catch (ISOException e) {
+            Logger.log (new LogEvent (this, "send-message-length", e));
+        }
+    }
+    protected int getMessageLength() throws IOException, ISOException {
+        byte[] b = new byte[2];
+        serverIn.readFully(b,0,2);
+        return Integer.parseInt (
+            ISOUtil.bcd2str (b, 0, 4, true)
+        );
+    }
+    protected void sendMessageHeader(ISOMsg m, int len) throws IOException { 
+        byte[] h = m.getHeader();
+        if (h != null) {
+            if (h.length == 5) {
+                // swap src/dest address
+                byte[] tmp = new byte[2];
+                System.arraycopy (h,   1, tmp, 0, 2);
+                System.arraycopy (h,   3,   h, 1, 2);
+                System.arraycopy (tmp, 0,   h, 3, 2);
+            }
+        }
+        else
+            h = header ;
+        if (h != null) 
+            serverOut.write(h);
+    }
+    /**
+     * New QSP compatible signature (see QSP's ConfigChannel)
+     * @param header String as seen by QSP
+     */
+    public void setHeader (String header) {
+        super.setHeader (ISOUtil.str2bcd(header, false));
+    }
+}
+
