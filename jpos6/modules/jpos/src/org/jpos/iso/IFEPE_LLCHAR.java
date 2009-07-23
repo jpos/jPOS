88787b809ab6d2f8ceaadbf2e5cb77cad7d12f84
Mark Salter <marksalter@talktalk.net>
2009-07-23 20:03:04 +0000

    MasterCard EBCDIC packager for fields like 48.

---
+++ jpos6/modules/jpos/src/org/jpos/iso/IFEPE_LLCHAR.java
@@ -0,0 +1,136 @@
+/*
+ * jPOS Project [http://jpos.org]
+ * Copyright (C) 2000-2009 Alejandro P. Revilla
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jpos.iso;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/*
+ * vim:set ts=8 sw=4:
+ */
+
+/**
+ * ISOFieldPackager EBCDIC variable len CHAR suitable for MasterCard subfield 48<br>
+ * <code>
+ * Format TTLL....
+ * Where TT is the 2 digit field number (Tag)
+ *       LL is the 2 digit field length
+ *       ... is the field content   
+ * </code>
+ * 
+ * @author <a href="mailto:eoin.flood@orbiscom.com">Eoin Flood</a>
+ * @author <a href="mailto:marksalter@talktalk.net">Mark Salter</a>
+ * 
+ * @version $Id: IFEP_LLCHAR.java 2706 2009-03-05 11:24:43Z apr $
+ * @see ISOComponent
+ */
+public class IFEPE_LLCHAR extends ISOFieldPackager {
+    public IFEPE_LLCHAR() {
+        super();
+    }
+
+    /**
+     * @param len
+     *            - field len
+     * @param description
+     *            symbolic descrption
+     */
+    public IFEPE_LLCHAR(int len, String description) {
+        super(len, description);
+    }
+
+    /**
+     * @param c
+     *            - a component
+     * @return packed component
+     * @exception ISOException
+     */
+    public byte[] pack(ISOComponent c) throws ISOException {
+        int len;
+        String s = (String) c.getValue();
+
+        if ((len = s.length()) > getLength() || len > 97) // paranoia settings
+            throw new ISOException("invalid len " + len
+                    + " packing LLEPCHAR field " + (Integer) c.getKey());
+
+        byte[] res = ISOUtil.asciiToEbcdic(ISOUtil.zeropad(((Integer) c
+                .getKey()).toString(), 2)
+                + ISOUtil.zeropad(Integer.toString(len ), 2) + s);
+
+        return res;
+    }
+
+    /**
+     * @param c
+     *            - the Component to unpack
+     * @param b
+     *            - binary image
+     * @param offset
+     *            - starting offset within the binary image
+     * @return consumed bytes
+     * @exception ISOException
+     */
+    public int unpack(ISOComponent c, byte[] b, int offset) throws ISOException {
+
+        byte[] length = new byte[2];
+        System.arraycopy(b, offset+2, length, 0, 2);
+        int len = Integer.parseInt(new String(ISOUtil.ebcdicToAscii(length)));
+
+        if (!(c instanceof ISOField))
+            throw new ISOException(c.getClass().getName()
+                    + " is not an ISOField");
+
+        String content = ISOUtil.ebcdicToAscii(b, offset + 4, len);
+
+        byte[] fieldId = new byte[2];
+        System.arraycopy(b, offset, fieldId, 0, 2);
+        int fieldNumber = Integer.parseInt(new String(ISOUtil
+                .ebcdicToAscii(fieldId)));
+
+        ((ISOField) c).setFieldNumber(fieldNumber);
+
+        c.setValue(content);
+
+        return len + 2 + 2;
+    }
+
+    public void unpack(ISOComponent c, InputStream in) throws IOException,
+            ISOException {
+
+        if (!(c instanceof ISOField))
+            throw new ISOException(c.getClass().getName()
+                    + " is not an ISOField");
+
+        byte[] fieldId = readBytes(in, 2);
+        int fieldNumber = Integer.parseInt(new String(ISOUtil
+                .ebcdicToAscii(fieldId)));
+
+        byte[] len = readBytes(in, 2);
+        int length = Integer.parseInt(new String(ISOUtil.ebcdicToAscii(len)));
+
+        String content = ISOUtil.ebcdicToAscii(readBytes(in, length));
+
+        ((ISOField) c).setFieldNumber(fieldNumber);
+        c.setValue(content.substring(2));
+    }
+
+    public int getMaxPackedLength() {
+        return getLength() + 2;
+    }
+}
---
+++ jpos6/modules/jpos/src/org/jpos/iso/packager/MasterCardEBCDICSubFieldPackager.java
@@ -0,0 +1,139 @@
+/*
+ * jPOS Project [http://jpos.org]
+ * Copyright (C) 2000-2009 Alejandro P. Revilla
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jpos.iso.packager;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+import org.jpos.iso.ISOBasePackager;
+import org.jpos.iso.ISOComponent;
+import org.jpos.iso.ISOException;
+import org.jpos.iso.ISOField;
+import org.jpos.iso.ISOPackager;
+import org.jpos.iso.ISOUtil;
+import org.jpos.util.LogEvent;
+import org.jpos.util.Logger;
+
+/**
+ * MasterCard EBCDIC SubField packager
+ * @author Eoin Flood
+ * @author Mark Salter
+ * @version $Revision: 2706 $ $Date: 2009-03-05 11:24:43 +0000 (Thu, 05 Mar 2009) $
+ * @see ISOPackager
+ * @see ISOBasePackager
+ * @see ISOComponent
+ *
+ * This packager can be used is to handle EBCDIC subfields
+ * such as field 48 for MasterCard.
+ */
+public class MasterCardEBCDICSubFieldPackager extends ISOBasePackager
+{ 
+    /** 
+     * Default constructor
+     */
+    public MasterCardEBCDICSubFieldPackager()
+    {
+        super();
+    }
+
+    /**
+     * Always return false
+     */
+    protected boolean emitBitMap()
+    {
+        return false;
+    }
+
+    public byte[] pack (ISOComponent c) throws ISOException {
+        try {
+            int len =0;
+            Hashtable tab = c.getChildren();
+            ArrayList l = new ArrayList();
+
+            // Handle first IF_CHAR field
+            ISOField f0 = (ISOField) tab.get (new Integer(0));
+            if (f0 != null) {
+                String s = (String) f0.getValue();
+                len += s.length();
+                l.add (fld[0].pack (f0));
+            }
+            for (int i =1; i<fld.length; i++) {
+                Object obj = tab.get (new Integer(i));
+                if (obj instanceof ISOField) {
+                    ISOField f = (ISOField) obj;
+                    byte[] b = fld[i].pack(f);
+                    len += b.length;
+                    l.add (b);
+                } 
+            }
+            int k=0;
+            byte[] d = new byte[len];
+            for (int i=0; i<l.size(); i++) {
+                byte[] b = (byte[]) l.get(i);
+                for (int j=0; j<b.length; j++)
+                    d[k++] = b[j];
+            }
+            return d;
+        }
+        catch (Exception ex)
+        {
+            throw new ISOException (ex);
+        }
+    }
+
+    public int unpack (ISOComponent m, byte[] b) throws ISOException
+    {
+        LogEvent evt = new LogEvent (this, "unpack");
+        // Unpack the IF_CHAR field
+        int consumed = 0;
+        ISOComponent c;
+        if (fld[0] != null) {
+            c = fld[0].createComponent(0);
+            consumed += fld[0].unpack (c, b, consumed);
+            m.set(c);
+        }
+
+        // Now unpack the fields
+        while (consumed < b.length) {
+            byte[] length = new byte[2];
+            System.arraycopy(b, consumed,length,0,2);
+            int i = Integer.parseInt(new String(ISOUtil.ebcdicToAscii(length)));
+
+            if (fld[i] == null)
+                throw new ISOException ("Unsupported sub-field " + i + " unpacking field " + m.getKey());
+
+            c = fld[i].createComponent(i);
+            consumed += fld[i].unpack (c, b, consumed);
+            if (logger != null) 
+            {
+                evt.addMessage ("<unpack fld=\"" + i 
+                    +"\" packager=\""
+                    +fld[i].getClass().getName()+ "\">");
+                    evt.addMessage ("  <value>" 
+                    +c.getValue().toString()
+                    + "</value>");
+                evt.addMessage ("</unpack>");
+            }
+            m.set(c);
+        }
+        Logger.log (evt);
+        return consumed;
+    }
+}
+
