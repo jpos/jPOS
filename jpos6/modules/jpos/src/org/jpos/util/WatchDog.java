6476a018651b351775f975c9c1fefaf548bff723
Alejandro Revilla <apr@jpos.org>
2009-08-10 14:58:14 +0000

    New helper class (good companion for Profiler)

---
+++ jpos6/modules/jpos/src/org/jpos/util/WatchDog.java
@@ -0,0 +1,49 @@
+/*
+ * jPOS Project [http://jpos.org]
+ * Copyright (C) 2000-2009 Alejandro P. Revilla
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jpos.util;
+
+import java.util.TimerTask;
+import org.jpos.util.Log;
+
+
+/**
+ * WatchDog will issue a warning message 
+ * if not canceled on time
+ */
+public class WatchDog extends TimerTask {
+    String message;
+    String logName;
+    String realm;
+    public WatchDog (long maxWait, String message) {
+        this.message = message;
+        this.logName = "Q2";
+        this.realm = "watchdog";
+        DefaultTimer.getTimer().schedule (this, maxWait);
+    }
+    public void setLogName (String logName) {
+        this.logName = logName;
+    }
+    public void setRealm (String realm) {
+        this.realm = realm;
+    }
+    public void run () {
+        Log.getLog (logName, realm).warn (message);
+    }
+}
+
