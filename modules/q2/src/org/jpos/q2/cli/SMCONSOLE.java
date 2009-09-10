8db98482aca2f862eb6ed55111bf2716ef827a11
Alejandro Revilla <apr@jpos.org>
2009-09-10 12:28:44 +0000

    added SMCONSOLE command (wraps org.jpos.security.jceadapter.Console)

---
+++ modules/q2/src/org/jpos/q2/cli/SMCONSOLE.java
@@ -0,0 +1,34 @@
+/*
+ * jPOS Project [http://jpos.org]
+ * Copyright (C) 2000-2009 Alejandro P. Revilla
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jpos.q2.cli;
+
+import org.jpos.q2.CLI;
+
+public class SMCONSOLE implements CLI.Command {
+    public void exec (CLI cli, String[] args) {
+        try {
+            String[] params = new String[args.length-1];
+            System.arraycopy (args, 1, params, 0, params.length);
+            org.jpos.security.jceadapter.Console.main (params);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
