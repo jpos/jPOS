ae7ff6ea7ce92e2b8f5ecbfc075c01bfc3892f9c
Alejandro Revilla <apr@jpos.org>
2009-11-28 00:41:01 +0000

    DebugFilter can be used to dump image data to jPOS audit log

---
+++ modules/jpos/src/org/jpos/iso/filter/DebugFilter.java
@@ -0,0 +1,43 @@
+/*
+ * jPOS Project [http://jpos.org]
+ * Copyright (C) 2000-2009 Alejandro P. Revilla
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jpos.iso.filter;
+
+import org.jpos.iso.RawIncomingFilter;
+import org.jpos.iso.ISOMsg;
+import org.jpos.iso.ISOChannel;
+import org.jpos.util.LogEvent;
+import org.jpos.util.Dumpable;
+
+public class DebugFilter implements RawIncomingFilter {
+    public ISOMsg filter(ISOChannel channel, ISOMsg m, byte[] header, byte[] image, LogEvent evt)
+            throws VetoException
+    {
+        if (evt != null) {
+            if (header != null)
+                evt.addMessage (new Dumpable("header", header));
+            if (image != null)
+                evt.addMessage (new Dumpable ("image", image));
+        }
+        return m;
+    }
+
+    public ISOMsg filter(ISOChannel channel, ISOMsg m, LogEvent evt) throws VetoException {
+        return m;
+    }
+}
