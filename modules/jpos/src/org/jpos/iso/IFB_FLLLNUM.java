df174448e5ac845122c458c326578a1d49578683
Alejandro Revilla <apr@jpos.org>
2009-12-15 13:44:19 +0000

    RIGHT_PADDER_F versions of IFB_NUMERIC and IFB_FLLLNUM.

---
+++ modules/jpos/src/org/jpos/iso/IFB_FLLLNUM.java
@@ -0,0 +1,56 @@
+/*
+ * jPOS Project [http://jpos.org]
+ * Copyright (C) 2000-2009 Alejandro P. Revilla
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jpos.iso;
+
+
+/**
+ * ISOFieldPackager Binary LLLNUM
+ *
+ * @author apr@jpos.org
+ * @see ISOComponent
+ */
+
+public class IFB_FLLLNUM extends ISOStringFieldPackager {
+    public IFB_FLLLNUM() {
+        super(NullPadder.INSTANCE, BCDInterpreter.RIGHT_PADDED_F, BcdPrefixer.LLL);
+    }
+    /**
+     * @param len - field len
+     * @param description symbolic descrption
+     */
+    public IFB_FLLLNUM(int len, String description, boolean isLeftPadded) {
+        super(len, description, NullPadder.INSTANCE,
+                isLeftPadded ? BCDInterpreter.LEFT_PADDED : BCDInterpreter.RIGHT_PADDED_F,
+                BcdPrefixer.LLL);
+        checkLength(len, 999);
+    }
+
+    public void setLength(int len)
+    {
+        checkLength(len, 999);
+        super.setLength(len);
+    }
+
+    /** Must override ISOFieldPackager method to set the Interpreter correctly */
+    public void setPad(boolean pad)
+    {
+        setInterpreter(pad ? BCDInterpreter.LEFT_PADDED : BCDInterpreter.RIGHT_PADDED_F);
+        this.pad = pad;
+    }
+}
