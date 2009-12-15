875faa8c9219ef216221823bb69ceb3d391bc8fa
Alejandro Revilla <apr@jpos.org>
2009-12-15 13:28:15 +0000

    Similar to IFB_LLNUM with RIGHT_PADDER_F. We need to support this from the GenericPackager configuration, probably changing the 'pad' atttribute to support something like 'left|right|right_f' instead of just true|false

---
+++ modules/jpos/src/org/jpos/iso/IFB_FLLNUM.java
@@ -0,0 +1,55 @@
+/*
+ * jPOS Project [http://jpos.org]
+ * Copyright (C) 2000-2009 Alejandro P. Revilla
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jpos.iso;
+
+/**
+ * ISOFieldPackager Binary LLNUM
+ *
+ * @author apr@jpos.org0
+ * @see ISOComponent
+ */
+public class IFB_FLLNUM extends ISOStringFieldPackager {
+    public IFB_FLLNUM() {
+        super(NullPadder.INSTANCE, BCDInterpreter.RIGHT_PADDED_F, BcdPrefixer.LL);
+    }
+    /**
+     * @param len - field len
+     * @param description symbolic descrption
+     */
+    public IFB_FLLNUM(int len, String description, boolean isLeftPadded) {
+        super(len, description, NullPadder.INSTANCE,
+                isLeftPadded ? BCDInterpreter.LEFT_PADDED : BCDInterpreter.RIGHT_PADDED_F,
+                BcdPrefixer.LL);
+        checkLength(len, 99);
+    }
+    
+    public void setLength(int len)
+    {
+        checkLength(len, 99);
+        super.setLength(len);
+    }
+
+    /** Must override ISOFieldPackager method to set the Interpreter correctly */
+    public void setPad (boolean pad)
+    {
+        setInterpreter(pad ? BCDInterpreter.LEFT_PADDED : BCDInterpreter.RIGHT_PADDED_F);
+        this.pad = pad;
+    }
+}
+
