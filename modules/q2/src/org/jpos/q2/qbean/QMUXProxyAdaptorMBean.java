21dfb2477b8ddf46ce8c2b096ac41c55b20e28a5
Mark Salter <marksalter@talktalk.net>
2010-04-20 09:35:52 +0000

    Provide QMUX exposure through RMI, to allow remote clients to address and use QMUX.

---
+++ modules/q2/src/org/jpos/q2/qbean/QMUXProxyAdaptorMBean.java
@@ -0,0 +1,33 @@
+/*
+ * jPOS Project [http://jpos.org]
+ * Copyright (C) 2000-2010 Alejandro P. Revilla
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jpos.q2.qbean;
+
+/**
+ * MBean interface.
+ * @author Mark Salter
+ * @author Alwyn Schoeman
+ * @version $Revision: 1.1.1.1 $ $Date: 2003/04/03 04:23:40 $
+ */
+public interface QMUXProxyAdaptorMBean extends org.jpos.q2.QBeanSupportMBean {
+
+    void setQmuxName(java.lang.String qmuxName) ;
+
+    java.lang.String getQmuxName() ;
+
+}
