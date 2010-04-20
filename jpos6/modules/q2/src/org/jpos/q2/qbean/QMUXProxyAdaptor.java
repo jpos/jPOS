34c5e25d804884d495e5d407fd8dc42ee2daafa1
Mark Salter <marksalter@talktalk.net>
2010-04-20 09:35:52 +0000

    Provide QMUX exposure through RMI, to allow remote clients to address and use QMUX.

---
+++ jpos6/modules/q2/src/org/jpos/q2/qbean/QMUXProxyAdaptor.java
@@ -0,0 +1,85 @@
+/*
+ * jPOS Project [http://jpos.org]
+ * Copyright (C) 2000-2010 Alejandro P. Revilla
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jpos.q2.qbean;
+
+import java.rmi.RemoteException;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.jpos.q2.QBeanSupport;
+import org.jpos.q2.iso.QMUX;
+import org.jpos.q2.iso.QMUXProxy;
+import org.jpos.util.NameRegistrar;
+import org.jpos.util.NameRegistrar.NotFoundException;
+
+/**
+ * Remote Space Proxy Adaptor.
+ *
+ * @author Mark Salter
+ * @author Alwyn Schoeman
+ * @version $Revision: 2854 $ $Date: 2010-01-02 10:34:31 +0000 (Sat, 02 Jan 2010) $
+ * @jmx:mbean description="QMUXProxy QBean"
+ *                  extends="org.jpos.q2.QBeanSupportMBean"
+ */
+
+public class QMUXProxyAdaptor 
+    extends QBeanSupport
+    implements QMUXProxyAdaptorMBean
+{
+    private QMUXProxy qmuxproxy = null;
+    private String qmuxName;
+    private QMUX qmux;
+
+    public QMUXProxyAdaptor () {
+        super ();
+    }
+
+    protected void startService () throws RemoteException, NamingException, NotFoundException {
+        qmux = (QMUX) NameRegistrar.get(qmuxName);
+        qmuxproxy = new QMUXProxy(qmux);
+        InitialContext ctx = new InitialContext ();
+        ctx.rebind (getName (), qmuxproxy);
+    }
+
+    protected void stopService () throws NamingException {
+        InitialContext ctx = new InitialContext ();
+        ctx.unbind(getName());
+    }
+    
+    /**
+     * @jmx:managed-attribute description="MUX Name"
+     */
+    public synchronized void setQmuxName (String muxName) {
+        this.qmuxName = muxName;
+        setAttr (getAttrs (), "qmuxName", muxName);
+        setModified (true);
+    }
+    
+   
+    /**
+     * @jmx:managed-attribute description="MUX Name"
+     */
+    public String getQmuxName () {
+        return qmuxName;
+    }
+
+}
+
+   
---
+++ jpos6/modules/q2mod_jpos/src/org/jpos/q2/iso/QMUXProxy.java
@@ -0,0 +1,94 @@
+/*
+ * jPOS Project [http://jpos.org]
+ * Copyright (C) 2000-2010 Alejandro P. Revilla
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jpos.q2.iso;
+
+import org.jpos.core.Configuration;
+import org.jpos.core.ConfigurationException;
+import org.jpos.core.ReConfigurable;
+import org.jpos.iso.ISOException;
+import org.jpos.iso.ISOMsg;
+import org.jpos.iso.ISOResponseListener;
+import org.jpos.q2.qbean.RemoteQMUX;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.io.Serializable;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.*;
+import java.util.Set;
+
+/**
+ * RMI QMUX Proxy
+ * @author Mark Salter 
+ * @author Alejandro Revilla
+ * @author Niclas Hedhman
+ * @version $Revision: 2854 $ $Date: 2010-01-02 10:34:31 +0000 (Sat, 02 Jan 2010) $
+ * @since 1.4.9
+ */
+public class QMUXProxy implements RemoteQMUX, ReConfigurable {
+    
+    QMUX qmux;
+
+    Configuration cfg;
+    private RemoteRef ref;
+    private RemoteStub stub;
+    public QMUXProxy (QMUX qmux) throws RemoteException {
+        super();
+        this.qmux = qmux;
+        startService ();
+    }
+
+    private void startService () throws RemoteException 
+    {
+        try {
+            LocateRegistry.createRegistry (Registry.REGISTRY_PORT);
+        } catch (ExportException e) {
+            // registry already exists
+        }
+        stub = UnicastRemoteObject.exportObject (this);
+        ref  = stub.getRef();
+    }
+
+     @Override
+    public boolean isConnected() {
+        return qmux.isConnected();
+    }
+
+    @Override
+    public ISOMsg request(ISOMsg m, long timeout) throws ISOException {
+        return qmux.request(m, timeout);
+    }
+
+    @Override
+    public void request(ISOMsg m, long timeout, ISOResponseListener rl,
+            Object handBack) throws ISOException {
+        qmux.request(m, timeout, rl, handBack);
+        
+    }
+
+    @Override
+    public void setConfiguration(Configuration cfg)
+            throws ConfigurationException {
+        qmux.setConfiguration(cfg);
+        
+    }
+}
+
