15e0785efa4cb790c62016e0ea37cf5718fc2b8d
Alejandro Revilla <apr@jpos.org>
2010-04-28 12:27:45 +0000

    Initial take at TPS meter

---
+++ modules/jpos/test/org/jpos/util/TPSTestCase.java
@@ -0,0 +1,37 @@
+/*
+ * jPOS Project [http://jpos.org]
+ * Copyright (C) 2000-2010 Alejandro P. Revilla
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jpos.util;
+
+import junit.framework.TestCase;
+
+public class TPSTestCase extends TestCase {
+    public void testTenTPS() throws Exception {
+        TPS tps = new TPS();
+        for (int i=0; i<1000; i++)
+            tps.tick();
+        Thread.sleep (1000L);
+        assertEquals("Expected 1000 TPS", 1000, tps.intValue());
+        assertEquals("Still expecting 1000 TPS on a second call", 1000, tps.intValue());
+        Thread.sleep (1000L);
+        assertEquals(
+            "TPS should be zero but it's "+tps.intValue() + " (" + tps.floatValue() + ")",
+            0, tps.intValue()
+        );
+    }
+}
