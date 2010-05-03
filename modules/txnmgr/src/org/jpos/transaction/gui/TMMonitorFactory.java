948f596ef007694d2970cf47f5e3365d701ff8c9
Alejandro Revilla <apr@jpos.org>
2010-05-03 01:38:07 +0000

    First take at a TMMonitor component and its factory

---
+++ modules/txnmgr/src/org/jpos/transaction/gui/TMMonitorFactory.java
@@ -0,0 +1,39 @@
+/*
+ * jPOS Project [http://jpos.org]
+ * Copyright (C) 2000-2010 Alejandro P. Revilla
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jpos.transaction.gui;
+
+import org.jpos.ui.UIFactory;
+import org.jpos.ui.UI;
+import org.jpos.transaction.TransactionManager;
+import org.jpos.util.NameRegistrar;
+import org.jdom.Element;
+
+import javax.swing.*;
+
+public class TMMonitorFactory implements UIFactory {
+    public JComponent create(UI ui, Element e) {
+        try {
+            TransactionManager tm =
+                (TransactionManager) NameRegistrar.get (e.getAttributeValue ("transaction-manager"));
+            return new TMMonitor (ui, tm);
+        } catch (NameRegistrar.NotFoundException ex) {
+            return new JTextArea (ex.toString());
+        }
+    }
+}
