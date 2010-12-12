76b5a522e8d3b0d6f2b32877dc1388679bdd24cd
Alejandro Revilla <apr@jpos.org>
2010-12-12 21:53:20 -0200

    add a performance test

--- jpos/src/test/java/org/jpos/iso/packagers/PackagerTestCase.java
+++ jpos/src/test/java/org/jpos/iso/packagers/PackagerTestCase.java
@@ -19,16 +19,20 @@
 package org.jpos.iso.packagers;
 
 import junit.framework.TestCase;
+import org.jpos.iso.ISODate;
 import org.jpos.iso.ISOMsg;
 import org.jpos.iso.ISOPackager;
 import org.jpos.iso.TestUtils;
 import org.jpos.iso.packager.*;
+import org.jpos.util.Profiler;
+import org.jpos.util.TPS;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.Arrays;
+import java.util.Date;
 
 public class PackagerTestCase extends TestCase {
     private XMLPackager xmlPackager;
@@ -104,10 +108,75 @@ public class PackagerTestCase extends TestCase {
     public void testXMLPackager () throws Exception {
         doTest (xmlPackager, "XMLPackager", "XMLPackager");
     }
-    
+
     public void testGeneric93ebcdic() throws Exception {
         doTest (new GenericPackager ("src/main/resources/cfg/packager/iso93ebcdic-custom.xml"), "ISO93ebcdic-Custom-XmlMsg", "ISO93ebcdic-Custom-Img");        
-}    
+    }
+    public void testPerformance() throws Exception {
+        final int COUNT = 100000;
+        ISOPackager p = new GenericPackager ("src/main/resources/cfg/packager/iso87binary.xml");
+        ISOMsg baseMsg = getMsg("ISO87");
+        System.out.println ("\n--- pack/unpack performance test ---\n");
+        Profiler prof = new Profiler();
+        TPS tps = new TPS(true);
+        for (int i=0; i<COUNT; i++) {
+            pack (baseMsg, p);
+            tps.tick();
+        }
+        prof.checkPoint ("PACK " + tps.toString());
+
+        byte[] buf = baseMsg.pack();
+        tps = new TPS(true);
+        for (int i=0; i<COUNT; i++) {
+            unpack (buf, p);
+            tps.tick();
+        }
+        prof.checkPoint ("UNPACK " + tps.toString());
+
+        tps = new TPS(true);
+        for (int i=0; i<COUNT; i++) {
+            pack (baseMsg, p);
+            unpack (buf, p);
+            tps.tick();
+        }
+        prof.checkPoint ("PACK/UNPACK " + tps.toString());
+        tps = new TPS(true);
+        for (int i=0; i<COUNT; i++) {
+            updatePackAndUnpack(baseMsg, p);
+            tps.tick();
+        }
+        prof.checkPoint ("UPDATE/PACK/UNPACK " + tps.toString());
+
+        prof.dump(System.out, "");
+        System.out.println ("");
+    }
+    private void pack (ISOMsg m, ISOPackager p) throws Exception {
+        m.setPackager (p);
+        m.pack();
+    }
+    private void unpack (byte[] buf, ISOPackager p) throws Exception {
+        ISOMsg m = new ISOMsg();
+        m.setPackager (p);
+        m.unpack (buf);
+    }
+
+    private ISOMsg updatePackAndUnpack (ISOMsg m, ISOPackager p) throws Exception {
+        Date now = new Date();
+        m.setPackager (p);
+        m.set(7, ISODate.getDateTime(now));
+        m.set (12, ISODate.formatDate(now, "HHmmss"));
+        m.set (13, ISODate.formatDate (now, "MMdd"));
+
+        int stan = Integer.parseInt(m.getString(11)) % 1000000;
+        if (++stan == 0L) stan++;
+        m.set (11, Integer.toString(stan));
+        m.set (4, Integer.toString (stan));
+
+        ISOMsg m1 = new ISOMsg();
+        m1.setPackager(p);
+        m1.unpack (m.pack());
+        return m1;
+    }
     private void doTest (ISOPackager packager, String msg, String img)
         throws Exception
     {
@@ -140,5 +209,35 @@ public class PackagerTestCase extends TestCase {
         m2.unpack (new ByteArrayInputStream (out.toByteArray()));
         TestUtils.assertEquals(b, m2.pack());
     }
+
+    /*
+    private void doPack (ISOMsg ISOPackager packager, String msg, String img)
+        throws Exception
+    {
+        // Logger logger = new Logger();
+        // logger.addListener (new SimpleLogListener (System.out));
+        // packager.setLogger (logger, msg + "-m");
+
+        ISOMsg m = getMsg (msg);
+        m.setPackager (packager);
+        byte[] p = m.pack();
+
+        byte[] b = getImage (img);
+        TestUtils.assertEquals(b, p);
+
+        ISOMsg m1 = new ISOMsg ();
+        // packager.setLogger (logger, msg + "-m1");
+        m1.setPackager (packager);
+        m1.unpack (b);
+        TestUtils.assertEquals(b, m1.pack());
+
+        ISOMsg m2 = new ISOMsg ();
+        m2.setPackager (packager);
+        // packager.setLogger (logger, msg + "-m2");
+        m2.unpack (new ByteArrayInputStream (out.toByteArray()));
+        TestUtils.assertEquals(b, m2.pack());
+    }
+    */
+
 }
 
