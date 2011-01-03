558638875b28293533607bf30b112859ff37aefe
Alejandro Revilla <apr@jpos.org>
2011-01-03 15:15:44 -0200

    reversed automatic license change

--- jpos/src/main/java/org/jpos/q2/CLIClassNameCompletor.java
+++ jpos/src/main/java/org/jpos/q2/CLIClassNameCompletor.java
@@ -1,19 +1,9 @@
 /*
- * jPOS Project [http://jpos.org]
- * Copyright (C) 2000-2011 Alejandro P. Revilla
+ * Copyright (c) 2002-2007, Marc Prud'hommeaux. All rights reserved.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * This software is distributable under the BSD license. See the terms of the
+ * BSD license in the documentation provided with this software.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package org.jpos.q2;
