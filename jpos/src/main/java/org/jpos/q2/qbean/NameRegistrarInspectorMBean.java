01e12cfd7fe2cd8f8de06a931e3ab846e07f4e8c
Alejandro Revilla <apr@jpos.org>
2011-01-03 14:46:12 -0200

    Update copyright year to 2011

--- jpos/src/main/java/org/jpos/q2/qbean/NameRegistrarInspectorMBean.java
+++ jpos/src/main/java/org/jpos/q2/qbean/NameRegistrarInspectorMBean.java
@@ -1,6 +1,6 @@
 /*
  * jPOS Project [http://jpos.org]
- * Copyright (C) 2000-2010 Alejandro P. Revilla
+ * Copyright (C) 2000-2011 Alejandro P. Revilla
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
