fadc7076a232abe9b3f5acdd0adbc758959de212
Alejandro Revilla <apr@jpos.org>
2012-01-13 19:38:05 -0200

    reformatted - no code change

--- jpos/src/main/java/org/jpos/q2/iso/ChannelAdaptorMBean.java
+++ jpos/src/main/java/org/jpos/q2/iso/ChannelAdaptorMBean.java
@@ -18,43 +18,33 @@
 
 package org.jpos.q2.iso;
 
+import org.jpos.q2.QBeanSupportMBean;
+
 /**
  * MBean interface.
  * @author Alejandro Revilla
  * @version $Revision: 2241 $ $Date: 2006-01-23 11:27:32 -0200 (Mon, 23 Jan 2006) $
  */
-public interface ChannelAdaptorMBean extends org.jpos.q2.QBeanSupportMBean {
-
-  void setReconnectDelay(long delay) ;
-
-  long getReconnectDelay() ;
-
-  void setInQueue(java.lang.String in) ;
-
-  java.lang.String getInQueue() ;
-
-  void setOutQueue(java.lang.String out) ;
-
-  java.lang.String getOutQueue() ;
-
-  void setHost(java.lang.String host) ;
-
-  java.lang.String getHost() ;
-
-  void setPort(int port) ;
-
-  int getPort() ;
-
-  void setSocketFactory(java.lang.String sFac) ;
-
-  java.lang.String getSocketFactory() ;
-
-  public boolean isConnected();
-  public void resetCounters ();
-  public String getCountersAsString ();
-  public int getTXCounter();
-  public int getRXCounter();
-  public int getConnectsCounter();
-  public long getLastTxnTimestampInMillis();
-  public long getIdleTimeInMillis();
+@SuppressWarnings("unused")
+public interface ChannelAdaptorMBean extends QBeanSupportMBean {
+    public void setReconnectDelay(long delay);
+    public long getReconnectDelay();
+    public void setInQueue(java.lang.String in);
+    public String getInQueue();
+    public void setOutQueue(java.lang.String out);
+    public String getOutQueue();
+    public void setHost(java.lang.String host);
+    public String getHost();
+    public void setPort(int port);
+    public int getPort();
+    public void setSocketFactory(java.lang.String sFac);
+    public String getSocketFactory();
+    public boolean isConnected();
+    public void resetCounters();
+    public String getCountersAsString();
+    public int getTXCounter();
+    public int getRXCounter();
+    public int getConnectsCounter();
+    public long getLastTxnTimestampInMillis();
+    public long getIdleTimeInMillis();
 }
