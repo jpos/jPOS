34654db98c6c5fd783953c066db0319601e4d84f
Alejandro Revilla <apr@jpos.org>
2013-03-10 16:23:15 -0300

    s/server-connection-listener/connection-listener

--- jpos/src/main/java/org/jpos/q2/iso/QServer.java
+++ jpos/src/main/java/org/jpos/q2/iso/QServer.java
@@ -1,6 +1,6 @@
 /*
  * jPOS Project [http://jpos.org]
- * Copyright (C) 2000-2012 Alejandro P. Revilla
+ * Copyright (C) 2000-2013 Alejandro P. Revilla
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -339,7 +339,7 @@ public class QServer
 
         QFactory factory = getFactory ();
         Iterator iter = getPersist().getChildren (
-            "server-connection-listener"
+            "connection-listener"
         ).iterator();
         while (iter.hasNext()) {
             Element l = (Element) iter.next();
