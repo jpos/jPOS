56696625a793b591f4cca7c97677612e80035cc8
Alejandro Revilla <apr@jpos.org>
2013-11-09 18:41:24 -0200

    Update GH-Pages

---
+++ .travis/update-gh-pages.sh
@@ -0,0 +1,28 @@
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo -e "Starting to update gh-pages\n"
+
+  cd `dirname $0`/..
+
+  #copy data we're interested in to other place
+  cp -R jPOS/build/reports/tests $HOME/tests
+
+  #go to home and setup git
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis"
+
+  #using token clone gh-pages branch
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/jpos/jPOS.git gh-pages > /dev/null 2>&1
+
+  #go into diractory and copy data we're interested in to that directory
+  cd gh-pages
+  cp -Rf $HOME/tests/* .
+
+  #add, commit and push files
+  git add -f .
+  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+  git push -fq origin gh-pages > /dev/null
+
+  echo -e "Done updating gh-pages\n"
+fi
+
