377cbe8eb1b6f031f2f98de3c74d32d85813548a
Murtuza Chhil <chillum@gmail.com>
2014-02-25 22:52:42 +0530

    Prevent multiple instances from running
    https://jpos.org/issues/issue/jPOS-100

---
+++ jpos/src/main/java/org/jpos/q2/qbean/QSingleInstancePortBasedManager.java
@@ -0,0 +1,51 @@
+package org.jpos.q2.qbean;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+import org.jpos.core.Configuration;
+import org.jpos.core.ConfigurationException;
+import org.jpos.q2.QBeanSupport;
+
+public class QSingleInstancePortBasedManager extends QBeanSupport {
+
+    ServerSocket ss;
+
+    int          port;
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.jpos.q2.QBeanSupport#initService()
+     */
+    @Override
+    protected void initService() throws Exception {
+
+        try {
+            // attempt to bind, if another instance is already bound, an
+            // exception will get throwm
+            ss = new ServerSocket(port);
+        }
+        catch (IOException e) {
+            getLog().error("An instance of switch is already running. Shutting this instance");
+            getServer().shutdown();
+        }
+
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see
+     * org.jpos.q2.QBeanSupport#setConfiguration(org.jpos.core.Configuration)
+     */
+    @Override
+    public void setConfiguration(Configuration cfg) throws ConfigurationException {
+
+        // TODO Auto-generated method stub
+        super.setConfiguration(cfg);
+
+        port = cfg.getInt("port", 65000);
+    }
+
+}
