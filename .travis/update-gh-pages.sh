3e9264e880cef7acf444b46710a9e898ddb4e13b
Alejandro Revilla <apr@jpos.org>
2014-04-02 19:27:25 -0300

    pmd directory already exists, use -p

--- .travis/update-gh-pages.sh
+++ .travis/update-gh-pages.sh
@@ -17,7 +17,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   #go into diractory and copy data we're interested in to that directory
   cd gh-pages
   cp -Rf $HOME/tests/* .
-  mkdir pmd && cp -Rf $HOME/pmd/* pmd
+  mkdir -p pmd && cp -Rf $HOME/pmd/* pmd
 
   #add, commit and push files
   git add -f .
