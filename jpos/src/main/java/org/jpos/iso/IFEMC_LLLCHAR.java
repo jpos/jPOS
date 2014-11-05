94d6c0698fb26cc6ca495cd0fc53b96da78f6fe0
Alejandro Revilla <apr@jpos.org>
2014-11-04 22:52:38 -0200

    LLL version of IFEMC_LLCHAR

---
+++ jpos/src/main/java/org/jpos/iso/IFEMC_LLLCHAR.java
@@ -0,0 +1,34 @@
+/*
+ * jPOS Project [http://jpos.org]
+ * Copyright (C) 2000-2014 Alejandro P. Revilla
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jpos.iso;
+
+public class IFEMC_LLLCHAR extends ISOTagStringFieldPackager {
+    public IFEMC_LLLCHAR() {
+        super(0, null, AsciiPrefixer.LLL, NullPadder.INSTANCE,
+                EbcdicInterpreter.INSTANCE, AsciiPrefixer.LLL);
+    }
+    /**
+     * @param len - field len
+     * @param description symbolic descrption
+     */
+    public IFEMC_LLLCHAR (int len, String description) {
+        super(len, description, AsciiPrefixer.LLL, NullPadder.INSTANCE,
+                EbcdicInterpreter.INSTANCE, AsciiPrefixer.LLL);
+    }
+}
