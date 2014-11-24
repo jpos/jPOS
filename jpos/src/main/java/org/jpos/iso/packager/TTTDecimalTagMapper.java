e9defcf63f435f6389d4a57fe5ea84945c6aef7d
Alejandro Revilla <apr@jpos.org>
2014-11-24 20:16:13 -0200

    Added TTTLLLV support
    We keep finding different ways people implement TLV

---
+++ jpos/src/main/java/org/jpos/iso/packager/TTTDecimalTagMapper.java
@@ -0,0 +1,38 @@
+/*
+ * jPOS Project [http://jpos.org]
+ * Copyright (C) 2000-2014 Alejandro P. Revilla
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jpos.iso.packager;
+
+/**
+ * TagMapper provides mappings between three decimal tags
+ * {@code TTT} and subfields.
+ *
+ * <p>Example of mappings:
+ * <ul>
+ *   <li>01 &lt;-&gt; 1
+ *   <li>23 &lt;-&gt; 23
+ * </ul>
+ *
+ * @author Michał Wiercioch, apr
+ */
+public class TTTDecimalTagMapper extends DecimalTagMapper {
+  public TTTDecimalTagMapper() {
+    super(3);
+  }
+}
+
