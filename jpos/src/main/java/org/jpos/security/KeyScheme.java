23f5bbcf524c7cd7f5d9db4d3283a6e08d3e2470
Robert Demski <drdemsey@gmail.com>
2015-04-27 00:14:37 +0200

    Add javadoc in KeyScheme

--- jpos/src/main/java/org/jpos/security/KeyScheme.java
+++ jpos/src/main/java/org/jpos/security/KeyScheme.java
@@ -19,10 +19,41 @@
 package org.jpos.security;
 
 /**
+ * Key Encription Scheme.
+ *
  * @author Robert Demski
- * @version $Revision$ $Date$
  */
 public enum KeyScheme {
-    Z,X,U,Y,T
+
+    /**
+     * Encryption of a single length DES key using X9.17 methods.
+     * <p>
+     * Used for encryption of keys under a variant LMK..
+     */
+    Z,
+
+    /**
+     * Encryption of a double length key using X9.17 methods.
+     */
+    X,
+
+    /**
+     * Encryption of a double length DES key using the variant method.
+     * <p>
+     * Used for encryption of keys under a variant LMK.
+     */
+    U,
+
+    /**
+     * Encryption of a triple length key using X9.17 methods.
+     */
+    Y,
+
+    /**
+     * Encryption of a triple length DES key using the variant method.
+     * <p>
+     * Used for encryption of keys under a variant LMK.
+     */
+    T
 }
 
