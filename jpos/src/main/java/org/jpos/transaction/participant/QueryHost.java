2cc2349d38b9496ffe7c0240e4eda090779ed271
Alejandro Revilla <apr@jpos.org>
2017-04-21 11:48:56 -0300

    new QueryHost participant

---
+++ jpos/src/main/java/org/jpos/transaction/participant/QueryHost.java
@@ -0,0 +1,131 @@
+/*
+ * jPOS Project [http://jpos.org]
+ * Copyright (C) 2000-2017 jPOS Software SRL
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jpos.transaction.participant;
+
+import java.io.Serializable;
+
+import org.jpos.core.Configurable;
+import org.jpos.core.Configuration;
+import org.jpos.core.ConfigurationException;
+import org.jpos.iso.*;
+import org.jpos.rc.CMF;
+import org.jpos.rc.Result;
+import org.jpos.transaction.ContextConstants;
+import org.jpos.transaction.TransactionParticipant;
+import org.jpos.util.Caller;
+import org.jpos.util.Chronometer;
+import org.jpos.util.NameRegistrar;
+import org.jpos.transaction.Context;
+
+@SuppressWarnings("unused")
+public class QueryHost implements TransactionParticipant, ISOResponseListener, Configurable {
+    private static final long DEFAULT_TIMEOUT = 30000L;
+    private static final long DEFAULT_WAIT_TIMEOUT = 12000L;
+
+    private long timeout;
+    private long waitTimeout;
+    private String requestName;
+    private String responseName;
+    private String destination;
+    private boolean continuations;
+    private Configuration cfg;
+    private String request;
+
+    public QueryHost () {
+        super();
+    }
+    public int prepare (long id, Serializable ser)  {
+        Context ctx = (Context) ser;
+
+        Result result = ctx.getResult();
+        String ds = ctx.getString(destination);
+        if (ds == null) {
+            return result.fail(
+              CMF.MISCONFIGURED_ENDPOINT, Caller.info(), "'%s' not present in Context", destination
+            ).FAIL();
+        }
+        String muxName = cfg.get ("mux." + ds , "mux." + ds);
+        MUX mux =  (MUX) NameRegistrar.getIfExists (muxName);
+        if (mux == null)
+            return result.fail(CMF.MISCONFIGURED_ENDPOINT, Caller.info(), "MUX '%s' not found", muxName).FAIL();
+
+        ISOMsg m = (ISOMsg) ctx.get (requestName);
+        if (m == null)
+            return result.fail(CMF.INVALID_REQUEST, Caller.info(), "'%s' is null", requestName).FAIL();
+
+        Chronometer chronometer = new Chronometer();
+        if (isConnected(mux)) {
+            long t = Math.max(timeout - chronometer.elapsed(), 1000L); // give at least a second to catch a response
+            try {
+                if (continuations) {
+                    mux.request(m, t, this, ctx);
+                    return PREPARED | READONLY | PAUSE | NO_JOIN;
+                } else {
+                    ISOMsg resp = mux.request(m, t);
+                    if (resp != null) {
+                        ctx.put(responseName, resp);
+                        return PREPARED | READONLY | NO_JOIN;
+                    } else {
+                        return result.fail(CMF.HOST_UNREACHABLE, Caller.info(), "'%s' does not respond", muxName).FAIL();
+                    }
+                }
+            } catch (ISOException e) {
+                return result.fail(CMF.SYSTEM_ERROR, Caller.info(), e.getMessage()).FAIL();
+            }
+        } else {
+            return result.fail(CMF.HOST_UNREACHABLE, Caller.info(), "'%s' is null", muxName).FAIL();
+        }
+
+    }
+
+    public void responseReceived (ISOMsg resp, Object handBack) {
+        Context ctx = (Context) handBack;
+        ctx.put (responseName, resp);
+        ctx.resume();
+    }
+    public void expired (Object handBack) {
+        Context ctx = (Context) handBack;
+        ctx.resume();
+    }
+    public void setConfiguration (Configuration cfg) throws ConfigurationException {
+        this.cfg = cfg;
+        timeout = cfg.getLong ("timeout", DEFAULT_TIMEOUT);
+        waitTimeout = cfg.getLong ("wait-timeout", DEFAULT_WAIT_TIMEOUT);
+        requestName = cfg.get ("request", ContextConstants.REQUEST.toString());
+        responseName = cfg.get ("response", ContextConstants.RESPONSE.toString());
+        destination = cfg.get ("destination", ContextConstants.DESTINATION.toString());
+        continuations = cfg.getBoolean("continuations", true);
+    }
+
+    public String getMuxName (String kki) {
+        return "mux." + cfg.get ("mux", kki.toLowerCase() + "-mux");
+    }
+
+    protected boolean isConnected (MUX mux) {
+        if (mux.isConnected())
+            return true;
+        long timeout = System.currentTimeMillis() + waitTimeout;
+        while (System.currentTimeMillis() < timeout) {
+            if (mux.isConnected())
+                return true;
+            ISOUtil.sleep (500);
+        }
+        return false;
+    }
+}
---
+++ jpos/src/test/java/org/jpos/transaction/participant/QueryHostTest.java
@@ -0,0 +1,134 @@
+/*
+ * jPOS Project [http://jpos.org]
+ * Copyright (C) 2000-2017 jPOS Software SRL
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jpos.transaction.participant;
+
+
+import org.jpos.core.Configuration;
+import org.jpos.core.SimpleConfiguration;
+import org.jpos.iso.*;
+import org.jpos.rc.CMF;
+import org.jpos.transaction.Context;
+import org.jpos.transaction.ContextConstants;
+import org.jpos.transaction.TransactionConstants;
+import org.jpos.util.NameRegistrar;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+public class QueryHostTest implements TransactionConstants, MUX {
+    private QueryHost queryHost;
+    private Configuration cfg;
+    private long stan;
+
+    @Before
+    public void setUp() throws Exception {
+        cfg = new SimpleConfiguration();
+        queryHost = new QueryHost();
+        queryHost.setConfiguration(cfg);
+        NameRegistrar.register("mux.TEST", this);
+    }
+
+    @Test
+    public void testSimpleQueryAsync() throws Exception {
+        Context ctx = new Context();
+        cfg.put("continuations", "yes");
+        queryHost.setConfiguration(cfg);
+        ctx.put(ContextConstants.REQUEST.toString(), createDummyRequest());
+        ctx.put(ContextConstants.DESTINATION.toString(), "TEST");
+        int action = queryHost.prepare(1L, ctx);
+        assertTrue(action == (PREPARED | READONLY | PAUSE | NO_JOIN));
+        assertNotNull (ctx.get(ContextConstants.RESPONSE.toString(), 1000));
+        assertFalse ("Should not have failures", ctx.getResult().hasFailures());
+    }
+
+    @Test
+    public void testSimpleQuerySync() throws Exception {
+        Context ctx = new Context();
+        cfg.put("continuations", "no");
+        queryHost.setConfiguration(cfg);
+        ctx.put(ContextConstants.REQUEST.toString(), createDummyRequest());
+        ctx.put(ContextConstants.DESTINATION.toString(), "TEST");
+        int action = queryHost.prepare(1L, ctx);
+        assertTrue(action == (PREPARED | READONLY | NO_JOIN));
+        assertNotNull (ctx.get(ContextConstants.RESPONSE.toString(), 1000));
+        assertFalse ("Should not have failures", ctx.getResult().hasFailures());
+    }
+
+    @Test
+    public void testNoRequest() throws Exception {
+        Context ctx = new Context();
+        cfg.put("continuations", "no");
+        queryHost.setConfiguration(cfg);
+        ctx.put(ContextConstants.DESTINATION.toString(), "TEST");
+        int action = queryHost.prepare(1L, ctx);
+        assertTrue(action == FAIL);
+        assertNull (ctx.get(ContextConstants.RESPONSE.toString()));
+        assertTrue ("has Failures", ctx.getResult().hasFailures());
+        assertTrue (ctx.getResult().failure().getIrc() == CMF.INVALID_REQUEST);
+    }
+
+    @Test
+    public void testNoDestination() throws Exception {
+        Context ctx = new Context();
+        cfg.put("continuations", "no");
+        queryHost.setConfiguration(cfg);
+        ctx.put(ContextConstants.REQUEST.toString(), createDummyRequest());
+        int action = queryHost.prepare(1L, ctx);
+        assertTrue(action == FAIL);
+        assertNull (ctx.get(ContextConstants.RESPONSE.toString()));
+        assertTrue ("has Failures", ctx.getResult().hasFailures());
+        assertTrue (ctx.getResult().failure().getIrc() == CMF.MISCONFIGURED_ENDPOINT);
+    }
+
+    @Override
+    public ISOMsg request(ISOMsg m, long timeout) throws ISOException {
+        ISOMsg r = (ISOMsg) m.clone();
+        r.setResponseMTI();
+        r.set(39, "00");
+        return r;
+    }
+
+    @Override
+    public void request(ISOMsg m, long timeout, ISOResponseListener rl, Object handBack) throws ISOException {
+        ISOMsg r = (ISOMsg) m.clone();
+        r.setResponseMTI();
+        r.set(39, "00");
+        rl.responseReceived(r, handBack);
+    }
+
+    @Override
+    public void send(ISOMsg m) throws IOException, ISOException { }
+
+    @Override
+    public boolean isConnected() {
+        return true;
+    }
+
+    private ISOMsg createDummyRequest() {
+        ISOMsg m = new ISOMsg("0800");
+        m.set(7, ISODate.getANSIDate(new Date()));
+        m.set(11, ISOUtil.zeropad(++stan, 6));
+        m.set(70, "301");
+        return m;
+    }
+}
