7a670e646a6ecaa17f8aadc3e468120ca7d0f10e
Alejandro Revilla <apr@jpos.org>
2017-09-21 14:45:23 -0300

    added compact Map<String,String serializer

--- jpos/src/test/java/org/jpos/util/SerializerTest.java
+++ jpos/src/test/java/org/jpos/util/SerializerTest.java
@@ -24,6 +24,8 @@ import static org.junit.Assert.assertNotSame;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -45,5 +47,15 @@ public class SerializerTest {
         assertNotNull(reconstituted);
         assertEquals(cal, reconstituted);
         assertNotSame(cal, reconstituted);
+    }
+
+    @Test
+    public void testStringMapSerializer() throws Exception {
+        Map<String,String> smap = new HashMap<>();
+        smap.put ("A", "ABC");
+        smap.put ("B", "CBA");
+        byte[] b = Serializer.serializeStringMap(smap);
+        Map<String,String> smap2 = Serializer.deserializeStringMap(b);
+        assertEquals(smap, smap2);
     }
 }
