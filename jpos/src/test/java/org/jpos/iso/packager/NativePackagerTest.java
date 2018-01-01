30d664cf9924edca8915ccec2191cf0dec7f525a
Alejandro Revilla <apr@jpos.org>
2018-01-01 12:09:30 -0300

    Upgrade Copyright year

--- jpos/src/test/java/org/jpos/iso/packager/NativePackagerTest.java
+++ jpos/src/test/java/org/jpos/iso/packager/NativePackagerTest.java
@@ -1,6 +1,6 @@
 /*
  * jPOS Project [http://jpos.org]
- * Copyright (C) 2000-2015 Alejandro P. Revilla
+ * Copyright (C) 2000-2018 jPOS Software SRL
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
