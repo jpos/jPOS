6d4a23e97446c1e4ab48d1a176442b2419dce568
Alejandro Revilla <apr@jpos.org>
2019-01-03 06:38:55 -0300

    Update Copyright year
    [ci skip]

--- jpos/src/test/java/org/jpos/iso/IFE_LLLNUMTest.java
+++ jpos/src/test/java/org/jpos/iso/IFE_LLLNUMTest.java
@@ -1,6 +1,6 @@
 /*
  * jPOS Project [http://jpos.org]
- * Copyright (C) 2000-2018 jPOS Software SRL
+ * Copyright (C) 2000-2019 jPOS Software SRL
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 
 package org.jpos.iso;
 
