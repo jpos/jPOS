jpos/jPOS

commit 1f4fea2d31ee07f2f53d8cdda21a5cae5290ca58

SimpleMsg improvements
* Improve printing when msgContend is Collection<Loggeable> or Loggeable[]
* Allowing the use of nullable msgName in order to produce output like:
   <tag>Some Value</tag>
* Add a constructor just with tagName and msgContent

jpos/src/main/java/org/jpos/util/SimpleMsg.java
@@ -21,6 +21,8 @@ package  org.jpos.util;
 import org.jpos.iso.ISOUtil;
 
 import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.Collection;
 
 /**
  * <p>
@@ -39,6 +41,10 @@ import java.io.PrintStream;
             this.tagName = tagName;
             this.msgName = msgName;
             this.msgContent = msgContent;
+        }
+
+        public SimpleMsg (String tagName, Object msgContent) {
+          this(tagName, null, msgContent);
         }
 
         public SimpleMsg (String tagName, String msgName, byte[] msgContent) {
@@ -67,20 +73,36 @@ import java.io.PrintStream;
          * @param indent indention string, usually suppiled by Logger
          * @see org.jpos.util.Loggeable
          */
+        @Override
         public void dump (PrintStream p, String indent) {
             String inner = indent + "  ";
             p.print(indent + "<" + tagName);
-            p.print(" name=\"" + msgName + "\"");
-            p.println(">");
-            if (msgContent instanceof SimpleMsg[]) {
-                // dump sub messages
-                for (int i = 0; i < ((SimpleMsg[])msgContent).length; i++)
-                    ((SimpleMsg[])msgContent)[i].dump(p, inner);
-            }
+            if (msgName != null)
+                p.print(" name=\"" + msgName + "\"");
+
+            Collection cl = null;
+            if (msgContent instanceof Object[])
+                cl = Arrays.asList((Object[]) msgContent);
+            else if (msgContent instanceof Collection)
+                cl = (Collection) msgContent;
             else if (msgContent instanceof Loggeable)
-                ((Loggeable)msgContent).dump(p, inner);
+                cl = Arrays.asList(msgContent);
+            else if (msgName != null)
+                cl = Arrays.asList(msgContent);
             else
-                p.println(inner + msgContent.toString());
-            p.println(indent + "</" + tagName + ">");
+                p.print(">" + msgContent.toString());
+
+            if (cl != null) {
+                p.println(">");
+                for (Object o : cl) {
+                    if (o instanceof Loggeable)
+                        ((Loggeable) o).dump(p, inner);
+                    else
+                        p.println(inner + o.toString());
+                }
+                p.print(indent);
+            }
+
+            p.println("</" + tagName + ">");
         }
     }

commit a6087682b0b7f9fcc4b29458d94e22fba089db84

Improve SimpleMsg exceptions logging
Introduced in 1f4fea2 logging of exceptions was too general and did not provide
significant details to solve the problem in the event of unexpected exceptions.

jpos/src/main/java/org/jpos/util/SimpleMsg.java
@@ -18,6 +18,9 @@
 
 package  org.jpos.util;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import org.jpos.iso.ISOUtil;
 
 import java.io.PrintStream;
@@ -31,8 +34,12 @@ import java.util.Collection;
  * @author Hani S. Kirollos
  * @version $Revision$ $Date$
  */
-    public class SimpleMsg
-            implements Loggeable {
+    public class SimpleMsg implements Loggeable {
+
+        static final String STACKTRACE_EXTRA_INDENT = "    ";
+
+        static final String STACKTRACE_TAB_REPLACE  = STACKTRACE_EXTRA_INDENT + STACKTRACE_EXTRA_INDENT;
+
         String tagName;
         String msgName;
         Object msgContent;
@@ -70,6 +77,8 @@ import java.util.Collection;
                 cl = (Collection) msgContent;
             else if (msgContent instanceof Loggeable)
                 cl = Arrays.asList(msgContent);
+            else if (msgContent instanceof Throwable)
+                cl = Arrays.asList(msgContent);
             else if (msgName != null && msgContent == null){
                 p.println("/>");
                 return;
@@ -87,6 +96,8 @@ import java.util.Collection;
                 for (Object o : cl) {
                     if (o instanceof Loggeable)
                         ((Loggeable) o).dump(p, inner);
+                    else if (o instanceof Throwable)
+                        p.print(formatThrowable(indent, (Throwable) o));
                     else
                         p.println(inner + o);
                 }
@@ -95,4 +106,24 @@ import java.util.Collection;
 
             p.println("</" + tagName + ">");
         }
+
+        private String formatThrowable(String indent, Throwable t) {
+            String inde = indent + STACKTRACE_EXTRA_INDENT;
+            try (
+                    OutputStream os = new ByteArrayOutputStream();
+                    PrintStream ps = new PrintStream(os);
+                ) {
+                    // indent stack trace first line
+                    ps.print(inde);
+                    t.printStackTrace(ps);
+
+                    String res = os.toString();
+                    res = res.replace("\n\t", "\n" + inde + STACKTRACE_TAB_REPLACE);
+                    res = res.replace("\nCaused by:", "\n" + inde + "Caused by:");
+                    return res;
+            } catch (IOException ex) {
+                return ""; // it should never occur for ByteArrayOutputStream
+            }
+        }
+
     }

jpos/src/test/java/org/jpos/util/SimpleMsgTest.java
@@ -19,9 +19,11 @@
 package org.jpos.util;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import org.hamcrest.Matchers;
 import org.jpos.iso.ISOUtil;
 import org.junit.Before;
 
@@ -266,6 +268,24 @@ public class SimpleMsgTest {
                       "--||--  null" + NL +
                       "--||--</tag>" +  NL
                       ,os.toString());
+    }
+
+    @Test
+    public void testDumpExceptionStackTrace() {
+        Exception ex = new IllegalArgumentException("This value is illegal"
+                , new IllegalStateException("Some illegal state")
+        );
+        Loggeable msg = new SimpleMsg("unexpected-exception", ex);
+
+        msg.dump(p, "--||--");
+        String res = os.toString();
+
+        assertThat(res, Matchers.startsWith("--||--<unexpected-exception>" + NL));
+        assertThat(res, Matchers.endsWith("--||--</unexpected-exception>" + NL));
+
+        assertThat(res, Matchers.containsString(NL + "--||--    java.lang.IllegalArgumentException: This value is illegal" + NL));
+        assertThat(res, Matchers.containsString(NL + "--||--    Caused by: java.lang.IllegalStateException: Some illegal state" + NL));
+        assertThat(res, Matchers.containsString(NL + "--||--            at org.jpos.util.SimpleMsgTest.test"));
     }
 
     @Test