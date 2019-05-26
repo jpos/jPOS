61c4a16b94a486c7118389927a2d34e91770aff2
Alejandro Revilla <apr@jpos.org>
2019-05-26 14:04:59 -0300

    Add IFB_LLLLCHAR field packager

---
+++ jpos/src/main/java/org/jpos/iso/IFB_LLLLCHAR.java
@@ -0,0 +1,47 @@
+/*
+ * jPOS Project [http://jpos.org]
+ * Copyright (C) 2000-2019 jPOS Software SRL
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jpos.iso;
+
+/**
+ * ISOFieldPackager Binary LLLCHAR
+ *
+ * @author apr@cs.com.uy
+ * @version $Id$
+ * @see ISOComponent
+ */
+public class IFB_LLLLCHAR extends ISOStringFieldPackager {
+    public IFB_LLLLCHAR() {
+        super(NullPadder.INSTANCE, AsciiInterpreter.INSTANCE, BcdPrefixer.LLLL);
+    }
+    /**
+     * @param len - field len
+     * @param description symbolic descrption
+     */
+    public IFB_LLLLCHAR(int len, String description) {
+        super(len, description, NullPadder.INSTANCE, AsciiInterpreter.INSTANCE, BcdPrefixer.LLLL);
+        checkLength(len, 9999);
+    }
+
+    public void setLength(int len)
+    {
+        checkLength(len, 9999);
+        super.setLength(len);
+    }
+}
+
