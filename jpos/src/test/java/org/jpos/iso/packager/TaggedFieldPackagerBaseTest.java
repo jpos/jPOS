jpos/jPOS

commit 686be859900f26efa428fba37de3007364caaa11

temporary fix to unsorted tags

jpos/src/main/java/org/jpos/iso/packager/GenericTaggedFieldsPackager.java
@@ -27,6 +27,7 @@ import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -56,6 +57,7 @@ public class GenericTaggedFieldsPackager extends GenericPackager
     @Override
     public int unpack(ISOComponent m, byte[] b) throws ISOException {
         LogEvent evt = new LogEvent(this, "unpack");
+        ISOFieldPackager[] fields = Arrays.copyOf(fld, fld.length);
         try {
             if (m.getComposite() != m)
                 throw new ISOException("Can't call packager on non Composite");
@@ -66,13 +68,17 @@ public class GenericTaggedFieldsPackager extends GenericPackager
 
             int consumed = 0;
             int maxField = fld.length;
-            for (int i = getFirstField(); i < maxField && consumed < b.length; i++) {
-                if (fld[i] != null) {
-                    ISOComponent c = fld[i].createComponent(i);
-                    int unpacked = fld[i].unpack(c, b, consumed);
-                    consumed = consumed + unpacked;
-                    if (unpacked > 0) {
-                        m.set(c);
+            while (consumed < b.length) {
+                for (int i = getFirstField(); i < maxField && consumed < b.length; i++) {
+                    if (fields[i] != null) {
+                        ISOComponent c = fields[i].createComponent(i);
+                        int unpacked = fields[i].unpack(c, b, consumed);
+                        consumed = consumed + unpacked;
+                        if (unpacked > 0) {
+                            if (!(fields[i] instanceof TaggedFieldPackagerBase))
+                                fields[i] = null;
+                            m.set(c);
+                        }
                     }
                 }
             }

commit 47d578f56aa82bf95d81689c97859f83c33d21f7

Test of unpacking undefined tag
It provides (disabled) test detecting 100% CPU as bug introduced in 686be85

jpos/src/test/java/org/jpos/iso/packager/TaggedFieldPackagerBaseTest.java
@@ -20,6 +20,7 @@ package org.jpos.iso.packager;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.jpos.iso.ISOMsg;
 import org.junit.jupiter.api.Test;
@@ -33,6 +34,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.jpos.iso.ISOPackager;
 import org.jpos.iso.ISOUtil;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 
 /**
  *
@@ -61,12 +63,24 @@ public class TaggedFieldPackagerBaseTest {
      */
     private static final String MESSAGE_VALUE2   = "06123456022A30748TagA3A10748TagA1012A100760TagA1";
 
+    /**
+     * Undefined DE48 tag.
+     * <p>
+     * After A1 <i>(id 1)</i> followed by undefined in packager A2 <i>(id 2)</i>.
+     */
+    private static final String MESSAGE_VALUE3   = "06123456022A10748TagA1A20748TagA2012A100760TagA1";
+
     private static final String REPR_MESSAGE1    = MESSAGE_MTI1 + toString(MESSAGE_BITMAP1) + MESSAGE_VALUE1;
 
     /**
      * Unsorted DE48 tags message representation.
      */
     private static final String REPR_MESSAGE2    = MESSAGE_MTI1 + toString(MESSAGE_BITMAP1) + MESSAGE_VALUE2;
+
+    /**
+     * Undefined DE48 tag message representation.
+     */
+    private static final String REPR_MESSAGE3    = MESSAGE_MTI1 + toString(MESSAGE_BITMAP1) + MESSAGE_VALUE3;
 
     private static ISOPackager packager;
 
@@ -129,6 +143,21 @@ public class TaggedFieldPackagerBaseTest {
             () -> assertEquals("123456", msg.getString(2)),
             () -> assertEquals("48TagA1", msg.getString("48.1")),
             () -> assertEquals("48TagA3", msg.getString("48.3")),
+            () -> assertEquals("60TagA1", msg.getString("60.1"))
+        );
+    }
+
+    @Disabled("It leads to 100% CPU usage")
+    @Test
+    public void testUnpackUndefinedTag() throws Exception {
+        ISOMsg msg = new ISOMsg();
+        packager.unpack(msg, toBytes(REPR_MESSAGE3));
+
+        assertAll(
+            () -> assertEquals("1100", msg.getString(0)),
+            () -> assertEquals("123456", msg.getString(2)),
+            () -> assertEquals("48TagA1", msg.getString("48.1")),
+            () -> assertNull(msg.getString("48.2")),
             () -> assertEquals("60TagA1", msg.getString("60.1"))
         );
     }

jpos/src/test/resources/org/jpos/iso/packagers/ISO93TLVPackager.xml
@@ -154,11 +154,6 @@
           name="Address Verification (AVS) Request Data"
           class="org.jpos.iso.IFA_TTLLCHAR"/>
       <isofield
-          id="2"
-          length="1"
-          name="Address Verification (AVS) Response Data"
-          class="org.jpos.iso.IFA_TTLLCHAR"/>
-      <isofield
           id="3"
           length="8"
           name="Generic Transaction Additional Data"