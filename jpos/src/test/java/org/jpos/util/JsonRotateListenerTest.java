59ce5514b145c5bd2668dbdafe15e6bddeae8eca
Erlangga <rangga_muslim@yahoo.co.id>
2019-10-19 22:53:36 +0700

    remove jackson-dataformat-xml

--- jpos/src/test/java/org/jpos/util/JsonRotateListenerTest.java
+++ jpos/src/test/java/org/jpos/util/JsonRotateListenerTest.java
@@ -167,8 +167,6 @@ public class JsonRotateListenerTest {
         try {
             new JSONObject(test);
         } catch (JSONException ex) {
-            // edited, to include @Arthur's comment
-            // e.g. in case JSONArray is valid as well...
             try {
                 new JSONArray(test);
             } catch (JSONException ex1) {
