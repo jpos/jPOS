28634c5dee770d9787e5d01a49eca3e3ddeae8d1
Alejandro Revilla <apr@jpos.org>
2020-01-02 14:01:10 -0300

    Update copyright year
    [ci skip]

--- jpos/src/test/java/org/jpos/util/SimpleLogSourceTest.java
+++ jpos/src/test/java/org/jpos/util/SimpleLogSourceTest.java
@@ -1,6 +1,6 @@
 /*
  * jPOS Project [http://jpos.org]
- * Copyright (C) 2000-2019 jPOS Software SRL
+ * Copyright (C) 2000-2020 jPOS Software SRL
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
