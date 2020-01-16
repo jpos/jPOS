dbfaca5f7b3c80d46ad743b978bec49c7173063f
Alwyn Schoeman <alwyn.schoeman@olspayments.com>
2020-01-16 13:06:49 -0500

    Support for LogEventWriters, allowing the customization of how LogEvent writes to the output stream

---
+++ jpos/src/main/java/org/jpos/util/LogEventWriter.java
@@ -0,0 +1,36 @@
+/*
+ * jPOS Project [http://jpos.org]
+ * Copyright (C) 2000-2020 jPOS Software SRL
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jpos.util;
+
+import java.io.PrintStream;
+
+/**
+ * LogEventWriters allow the customization of how LogListeners write LogEvent data to
+ * output streams.
+ *
+ * Its purpose is the formatting of output data and not the modification of individual events.
+ *
+ * @author Alwyn Schoeman
+ * @since 2.1.4
+ */
+public interface LogEventWriter {
+    void write(LogEvent ev);
+    void setPrintStream(PrintStream printStream);
+    void close();
+}
