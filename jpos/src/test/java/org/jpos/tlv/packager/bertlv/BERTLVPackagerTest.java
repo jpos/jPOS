8af204f5966ddacf6a0ef6a1efb7c489cc328ec9
Alejandro Revilla <apr@jpos.org>
2020-06-12 13:54:34 -0300

    add BERTLVPackager testZeroLength
    relates #315

---
+++ jpos/src/test/java/org/jpos/tlv/packager/bertlv/BERTLVPackagerTest.java
@@ -0,0 +1,42 @@
+/*
+ * jPOS Project [http://jpos.org]
+ * Copyright (C) 2000-2020 jPOS Software SRL
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jpos.tlv.packager.bertlv;
+
+import org.jpos.emv.EMVStandardTagType;
+import org.jpos.iso.*;
+import org.jpos.tlv.ISOTaggedField;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BERTLVPackagerTest {
+
+    @Test
+    public void testZeroLength() throws ISOException {
+        BERTLVPackager p = new BERTLVAsciiHexPackager();
+        ISOTaggedField t = new ISOTaggedField(EMVStandardTagType.AMOUNT_AUTHORISED_NUMERIC_0x9F02.getTagNumberHex(),
+          new ISOField(0, ""));
+        t.setFieldNumber(1);
+
+        ISOMsg m = new ISOMsg(55);
+        m.set(t);
+        byte[] b = p.pack(m, true, 1, 1);
+        assertArrayEquals(b, ISOUtil.hex2byte("394630323030"));
+    }
+}
