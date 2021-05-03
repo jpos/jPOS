3d72d7c095e08b0bb0b9da33c295428f34bc6823
Alejandro Revilla <apr@jpos.org>
2021-05-03 20:13:13 -0300

    target Java 16

---
+++ jpos/src/main/java/module-info.java
@@ -0,0 +1,37 @@
+module org.jpos.jpos {
+    requires java.se;
+    requires org.jdom2;
+    requires org.bouncycastle.pg;
+    requires org.bouncycastle.provider;
+    requires org.apache.commons.cli;
+    requires org.osgi.core;
+    // requires org.apache.sshd.core;
+    // requires org.apache.sshd.common;
+
+    exports org.jpos.iso.header;
+    exports org.jpos.iso.packager;
+    exports org.jpos.iso.validator;
+    exports org.jpos.q2;
+    exports org.jpos.q2.cli;
+    exports org.jpos.q2.install;
+    exports org.jpos.q2.iso;
+    exports org.jpos.q2.qbean;
+    exports org.jpos.q2.security;
+    exports org.jpos.q2.ui;
+    exports org.jpos.security;
+    exports org.jpos.security.jceadapter;
+    exports org.jpos.space;
+    exports org.jpos.tlv;
+    exports org.jpos.transaction;
+    exports org.jpos.transaction.gui;
+    exports org.jpos.transaction.participant;
+    exports org.jpos.ui;
+    exports org.jpos.ui.action;
+    exports org.jpos.ui.factory;
+    exports org.jpos.util;
+    exports org.jpos.util.function;
+    exports org.jpos.emv;
+    exports org.jpos.core.handlers.exception;
+    exports org.jpos.rc;
+}
+
