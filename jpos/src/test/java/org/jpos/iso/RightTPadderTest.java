b9438e673b7e8ca8ea19c8e806a155107d89500e
Alejandro Revilla <apr@jpos.org>
2023-06-05 23:01:54 -0300

    Update copyright year

--- jpos/src/test/java/org/jpos/iso/RightTPadderTest.java
+++ jpos/src/test/java/org/jpos/iso/RightTPadderTest.java
@@ -1,6 +1,6 @@
 /*
  * jPOS Project [http://jpos.org]
- * Copyright (C) 2000-2022 jPOS Software SRL
+ * Copyright (C) 2000-2023 jPOS Software SRL
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
