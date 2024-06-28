160e740492bd709bab060cf488a202c828e371d9
Alejandro Revilla <apr@jpos.org>
2024-06-28 10:04:40 -0300

    dump standard stacktrace

--- jpos/src/main/java/org/jpos/log/render/ThrowableSerializer.java
+++ jpos/src/main/java/org/jpos/log/render/ThrowableSerializer.java
@@ -4,19 +4,23 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 
 public class ThrowableSerializer extends JsonSerializer<Throwable> {
     @Override
-    public void serialize(Throwable value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+    public void serialize(Throwable t, JsonGenerator gen, SerializerProvider serializers) throws IOException {
         gen.writeStartObject();
-        gen.writeStringField("message", value.getMessage());
-        gen.writeArrayFieldStart("stackTrace");
-        StackTraceElement[] stackTrace = value.getStackTrace();
-        for (StackTraceElement stackTraceElement : stackTrace) {
-            gen.writeString(stackTraceElement.toString());
-        }
-        gen.writeEndArray();
+        gen.writeStringField("message", t.getMessage());
+        gen.writeStringField("stacktrace", toString(t));
         gen.writeEndObject();
+    }
+
+    private String toString (Throwable t) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(baos);
+        t.printStackTrace(ps);
+        return baos.toString();
     }
 }
