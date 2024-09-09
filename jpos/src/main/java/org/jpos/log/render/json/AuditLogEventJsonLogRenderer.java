cef2ee17c17a428dba8a00a40d475090d1eec504
Alejandro Revilla <apr@jpos.org>
2024-09-08 22:14:14 -0300

    Removed unused import

--- jpos/src/main/java/org/jpos/log/render/json/AuditLogEventJsonLogRenderer.java
+++ jpos/src/main/java/org/jpos/log/render/json/AuditLogEventJsonLogRenderer.java
@@ -2,7 +2,6 @@ package org.jpos.log.render.json;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.jpos.log.AuditLogEvent;
 import org.jpos.log.LogRenderer;
