9c5ba4f98d0586b3bec67fafaf78d9b22afc24c5
Alejandro Revilla <apr@jpos.org>
2026-01-04 14:40:45 -0300

    update copyright year

--- jpos/src/main/java/org/jpos/util/SnapshotLogListener.java
+++ jpos/src/main/java/org/jpos/util/SnapshotLogListener.java
@@ -1,7 +1,21 @@
-/**
- * SnapshotLogListener selectively logs events during a small time window,
- * allowing visibility during stress tests while minimizing overall log volume.
+/*
+ * jPOS Project [http://jpos.org]
+ * Copyright (C) 2000-2026 jPOS Software SRL
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package org.jpos.util;
 
 import org.jpos.core.Configurable;
