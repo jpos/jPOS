991400fdefcc701fabb162e36c0a88f1f419c41c
Alejandro Revilla <apr@jpos.org>
2026-04-20 13:17:46 -0300

    Fix path traversal in ENABLE/DISABLE CLI commands
    Validate that resolved file paths stay within the deploy directory
    by canonicalizing the path and checking it starts with deployDir.
    An input containing path traversal sequences (e.g. "../../etc/passwd")
    is now rejected.

--- jpos/src/main/java/org/jpos/q2/cli/deploy/DISABLE.java
+++ jpos/src/main/java/org/jpos/q2/cli/deploy/DISABLE.java
@@ -31,23 +31,25 @@ public class DISABLE implements CLICommand {
 	@Override
 	public void exec(CLIContext ctx, String[] args) throws Exception {
 		if (args.length < 2) {
-			ctx.println("Usage: enable <xml_file(no extenssion)>");
+			ctx.println("Usage: disable <xml_file(no extension)>");
 			return;
-		}		
-		File deployDir = ctx.getCLI().getQ2().getDeployDir();		
-		File deploy = new File(deployDir.getAbsolutePath()+"/"+args[1]+".xml");
-		if(deploy.exists() && deploy.isFile()){
-			File dest = new File(deployDir.getAbsolutePath()+"/"+args[1]+".xml.off");
+		}
+		File deployDir = ctx.getCLI().getQ2().getDeployDir();
+		File deploy = new File(deployDir, args[1] + ".xml").getCanonicalFile();
+		File dest = new File(deployDir, args[1] + ".xml.off").getCanonicalFile();
+		if (!deploy.toPath().startsWith(deployDir.getCanonicalFile().toPath())) {
+			ctx.println("Invalid path: " + args[1]);
+			return;
+		}
+		if (deploy.exists() && deploy.isFile()) {
 			deploy.renameTo(dest);
 			ctx.println("DISABLED: " + args[1]);
 			return;
 		}
-		File dest = new File(deployDir.getAbsolutePath()+"/"+args[1]+".xml.off");
-        if (dest.exists()) {
-            ctx.println("Already disabled: " + args[1]);
-            return;
-        }
-		ctx.println("Can't find the bean descriptor: "+args[1]);
-		return;
+		if (dest.exists()) {
+			ctx.println("Already disabled: " + args[1]);
+			return;
+		}
+		ctx.println("Can't find the bean descriptor: " + args[1]);
 	}
 }
