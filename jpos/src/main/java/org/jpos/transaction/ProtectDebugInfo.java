55335f5854b51f0da8e981bc3c73d879a762df9a
Barzilai Spinak <barspi@transactility.com>
2026-04-27 23:14:26 -0300

    ProtectDebugInfo: enhanced config

--- jpos/src/main/java/org/jpos/transaction/ProtectDebugInfo.java
+++ jpos/src/main/java/org/jpos/transaction/ProtectDebugInfo.java
@@ -29,44 +29,44 @@ import org.jpos.util.FSDMsg;
 import org.jpos.util.ProtectedLogListener;
 
 import java.io.Serializable;
-
-/**
- * @author Alejandro Revilla
- * @author David Bergert
- * @author Barspi
- */
+import java.util.Arrays;
 
 /**
  *Sample Usage:
  *<pre>
- *    &lt;participant class="org.jpos.transaction.ProtectDebugInfo" logger="Q2" realm="debug"&gt;
+ *    &lt;participant class="org.jpos.transaction.ProtectDebugInfo"&gt;
  *
  *        &lt;property name="protect-entry" value="REQUEST" /&gt;
  *        &lt;property name="protect-entry" value="RESPONSE" /&gt;
- *        &lt;property name="protect-entry" value="PAN" /&gt;
- *        &lt;property name="protect-entry" value="REQUEST_ICC_DATA" /&gt;
- *
- *        &lt;property name="wipe-entry" value="EXPDATE" /&gt;
+ *        &lt;property name="protect-entry" value="PAN, EXP, REQUEST_ICC_DATA" /&gt;
+ *        &lt;property name="wipe-entry"    value="EXPDATE" /&gt;
  *
  *        &lt;property name="protect-ISOMsg" value="2" /&gt;
- *        &lt;property name="protect-ISOMsg" value="35" /&gt;
- *        &lt;property name="protect-ISOMsg" value="45" /&gt;
- *        &lt;property name="wipe-ISOMsg" value="52" /&gt;
- *        &lt;property name="wipe-ISOMsg" value="55" /&gt;
+ *        &lt;property name="protect-ISOMsg" value="35, 45" /&gt;
+ *        &lt;property name="wipe-ISOMsg"    value="52, 55" /&gt;
  *
  *        &lt;property name="wipe-TLVList" value="0x56" /&gt;
  *        &lt;property name="wipe-TLVList" value="0x57" /&gt;
- *        &lt;property name="wipe-TLVList" value="0x5a" /&gt;
- *        &lt;property name="wipe-TLVList" value="0x5f20" /&gt;
+ *        &lt;property name="wipe-TLVList" value="0x5a, 0x5f20" /&gt;
  *
  *        &lt;property name="protect-FSDMsg" value="account-number" /&gt;
  *        &lt;property name="protect-FSDMsg" value="track2-data" /&gt;
+ *        &lt;property name="wipe-FSDMsg"    value="secret-key" /&gt;
  *
- *        &lt;/participant&gt;
+ *    &lt;/participant&gt;
  *</pre>
+ *
+ * Configuration properties admit comma/space-separated values, but can also be given in occurrences.
+ * All occurrences of the same property will be merged into a single list.
+ * <p>
+ * @author Alejandro Revilla
+ * @author David Bergert
+ * @author Barzilai Spinak
+ * </p>
  **/
 
- public class ProtectDebugInfo implements AbortParticipant, Configurable {
+public class ProtectDebugInfo implements AbortParticipant, Configurable {
+     private static final String COMMA_SPACE_SEPARATOR = "[,\\s]+";
      private String[] protectedEntries;
      private String[] wipedEntries;
      private String[] protectFSD;
@@ -86,14 +86,17 @@ import java.io.Serializable;
      public void abort  (long id, Serializable o) {
          protect ((Context) o);
      }
+
      private void protect (Context ctx) {
          /* wipe by removing entries from the context  */
          for (String s: wipedEntries)
              ctx.remove(s);
+
          /* Protect entry items */
          for (String s: protectedEntries) {
              Object o = ctx.get (s);
-             if (o instanceof ISOMsg){
+
+             if (o instanceof ISOMsg) {
                  ISOMsg m = ctx.get (s);
                  if (m != null) {
                      m = (ISOMsg) m.clone();
@@ -104,6 +107,7 @@ import java.io.Serializable;
                          wipeField(m,p);
                  }
              }
+
              if (o instanceof FSDMsg){
                  FSDMsg m = ctx.get (s);
                  if (m != null) {
@@ -126,16 +130,17 @@ import java.io.Serializable;
              }
          }
      }
+
      private void protectField (ISOMsg m, String f) {
          if (m != null) {
              m.set (f, protect (m.getString (f)));
          }
      }
+
     private void wipeField (ISOMsg m, String f) {
         if (m != null) {
-            Object v = null;
             try {
-                v = m.getValue(f);
+                Object v = m.getValue(f);
                 if (v != null) {
                     if (v instanceof String)
                         m.set(f, ProtectedLogListener.WIPED);
@@ -169,20 +174,30 @@ import java.io.Serializable;
                  m.set (f, ISOUtil.protect (s));
          }
      }
+
      private void wipeField (FSDMsg m, String f) {
          if (m != null && m.get(f) != null) {
              m.set (f, "*");
          }
      }
+
      private String protect (String s) {
          return s != null ? ISOUtil.protect (s) : s;
      }
+
      public void setConfiguration (Configuration cfg) throws ConfigurationException {
-         this.protectedEntries = cfg.getAll("protect-entry");
-         this.wipedEntries = cfg.getAll("wipe-entry");
-         this.protectFSD = cfg.getAll("protect-FSDMsg");
-         this.protectISO = cfg.getAll("protect-ISOMsg");
-         this.wipeISO = cfg.getAll("wipe-ISOMsg");
-         this.wipeTLV = cfg.getAll("wipe-TLVList");
+         this.protectedEntries = getValues(cfg, "protect-entry");
+         this.wipedEntries = getValues(cfg, "wipe-entry");
+         this.protectFSD = getValues(cfg, "protect-FSDMsg");
+         this.protectISO = getValues(cfg, "protect-ISOMsg");
+         this.wipeISO = getValues(cfg, "wipe-ISOMsg");
+         this.wipeTLV = getValues(cfg, "wipe-TLVList");
+     }
+     private String[] getValues (Configuration cfg, String name) {
+         return Arrays.stream(cfg.getAll(name))
+             .flatMap(v -> Arrays.stream(v.split(COMMA_SPACE_SEPARATOR)))
+             .map(String::trim)
+             .filter(v -> !v.isEmpty())
+             .toArray(String[]::new);
      }
 }
